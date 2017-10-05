52760a69ae404923ad44798ae459121bb337eff6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-10-04 22:23:41 -0400

    Made ExitChecker just reset the game instead of exiting

--- Assets/Scripts/ExitChecker.cs
+++ Assets/Scripts/ExitChecker.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class ExitChecker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (true || coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Player")
         {
-            Application.Quit();
+            GameManager.resetGame();
         }
     }
 }
