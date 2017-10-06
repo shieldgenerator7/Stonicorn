78cf00beb178e592211d13bf5c29f58aab2d0f23
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-10-05 22:33:50 -0400

    Added TapTrail tutorial for wall jump

---
+++ Assets/Scripts/TapTrail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTrail : MonoBehaviour {
+    //put a lot of these together to make a trail of tap target highlights
+
+    public GameObject nextTapStone;//the next piece of the trail
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (nextTapStone != null)
+            {
+                EffectManager.highlightTapArea(nextTapStone.transform.position);
+            }
+            else
+            {
+                EffectManager.highlightTapArea(Vector2.zero, false);
+            }
+        }
+    }
+}
