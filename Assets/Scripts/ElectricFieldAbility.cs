ad36202f0d04394b449725f0f92395e6246aeaac
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-11-17 15:22:35 -0500

    Electric Field slows things down

--- Assets/Scripts/ElectricFieldAbility.cs
+++ Assets/Scripts/ElectricFieldAbility.cs
@@ -10,6 +10,7 @@ public class ElectricFieldAbility : PlayerAbility
     private GameObject srii;//"shield range indicator instance"
     public float maxRange = 2.5f;
     public float maxHoldTime = 1;//how long until the max range is reached after it begins charging
+    public float maxSlowPercent = 0.90f;//the max percent of slowness applied to objects near the center of the field when the field has maxRange
     public float lastDisruptTime = 0;//the last time that something happened that disrupted the shield
     public float activationDelay = 2.0f;//how long after the last disruption the field can start regenerating
 
@@ -33,18 +34,25 @@ public class ElectricFieldAbility : PlayerAbility
 
     void FixedUpdate()
     {
-        //2017-01-24: copied from WeightSwitchActivator.FixedUpdate()
-        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, range);
-        for (int i = 0; i < hitColliders.Length; i++)
+        if (Time.time > lastDisruptTime + activationDelay)
         {
-            GameObject hc = hitColliders[i].gameObject;
-            if (!hc.Equals(gameObject))
+            //2017-01-24: copied from WeightSwitchActivator.FixedUpdate()
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, range);
+            for (int i = 0; i < hitColliders.Length; i++)
             {
+                GameObject hc = hitColliders[i].gameObject;
+
                 PowerConduit pc = hc.GetComponent<PowerConduit>();
                 if (pc != null && pc.convertsToEnergy)
                 {
                     //2017-11-17 FUTURE CODE: take out the 100 and put a variable in there, perhaps something to do with HP
                     float amountTaken = pc.convertSourceToEnergy(100, Time.fixedTime);
+                }
+
+                Rigidbody2D rb2d = hc.GetComponent<Rigidbody2D>();
+                if (rb2d != null)
+                {
+                    rb2d.velocity = rb2d.velocity * (1 - maxSlowPercent);
                 }
             }
         }
