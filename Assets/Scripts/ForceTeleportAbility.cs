6c705eadbeb659e5b3df52f4e17741f5d0ea4ead
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-15 15:24:32 -0500

    Fixed #361 Force Wave requires blastable to explode
    "blastable" is any GameObject with a RigidBody2D or Blastable component

--- Assets/Scripts/ForceTeleportAbility.cs
+++ Assets/Scripts/ForceTeleportAbility.cs
@@ -12,11 +12,11 @@ public class ForceTeleportAbility : PlayerAbility
     public float forceAmount = 10;//how much force to apply = forceAmount * 2^(holdTime*10)
     public float maxForce = 1000;//the maximum amount of force applied to one object
     public float maxRange = 3;
-    public float maxHoldTime = 1;//how long until the max range is reached
     public float maxSpeedBoost = 2;//how much force to give Merky when teleporting in a direction
 
     public float currentCharge = 0;//how much charge it has
     public float chargeIncrement = 0.1f;//how much to increment the charge by each teleport
+    public float maxCharge = 1;//the maximum amount of charge possible
     public float minChargeDecayDelay = 0.25f;//how much time (sec) of idleness before the charge starts decreasing
     public float maxChargeDecayDelay = 2.0f;
     public float chargeDecayRate = 0.4f;//how much charge decays per sec of idleness (after chargeDecayDelay)
@@ -61,9 +61,15 @@ public class ForceTeleportAbility : PlayerAbility
 
     public void charge(Vector2 oldPos, Vector2 newPos, Vector2 triedPos)
     {
-        if (Vector2.Distance(newPos, triedPos) < 0.5f || !GetComponent<PlayerController>().isOccupied(triedPos))
+        if (Vector2.Distance(newPos, triedPos) < 0.5f
+            //If there's a blastable in range, explode instead of charge
+            || !isBlastableInArea(triedPos, getRangeFromCharge(currentCharge)/2))
         {
             currentCharge += chargeIncrement;
+            if (currentCharge > maxCharge)
+            {
+                currentCharge = maxCharge;
+            }
             lastTeleportTime = Time.time;
         }
         else
@@ -74,23 +80,9 @@ public class ForceTeleportAbility : PlayerAbility
         }
     }
 
-    public new bool takesGesture()
-    {
-        return false;
-    }
-
-    public new bool takesHoldGesture()
-    {
-        return false;
-    }
-
     public override void processHoldGesture(Vector2 pos, float holdTime, bool finished)
     {
-        float range = maxRange * holdTime / maxHoldTime;
-        if (range > maxRange)
-        {
-            range = maxRange;
-        }
+        float range = getRangeFromCharge(holdTime);
         if (finished)
         {
             Collider2D[] hitColliders = Physics2D.OverlapCircleAll(pos, range);
@@ -139,6 +131,16 @@ public class ForceTeleportAbility : PlayerAbility
         }
     }
 
+    private float getRangeFromCharge(float charge)
+    {
+        float range = maxRange * charge / maxCharge;
+        if (range > maxRange)
+        {
+            range = maxRange;
+        }
+        return range;
+    }
+
     public override void dropHoldGesture()
     {
         if (frii != null)
@@ -150,7 +152,31 @@ public class ForceTeleportAbility : PlayerAbility
         //EffectManager.clearForceWaveShadows();
     }
 
-
+    /// <summary>
+    /// Returns whether or not an object with a Blastable or RigidBody component
+    /// is within the given range of the given position
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    bool isBlastableInArea(Vector2 pos, float range)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(pos, range);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Rigidbody2D orb2d = hitColliders[i].gameObject.GetComponent<Rigidbody2D>();
+            if (orb2d != null)
+            {
+                return true;
+            }
+            Blastable b = hitColliders[i].gameObject.GetComponent<Blastable>();
+            if(b != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
     void showExplosionEffect(Vector2 pos, float finalSize)
     {
