f8a4c752749e76a24c2b2f1c99972ee076184dff
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-31 00:17:30 -0500

    Added first version of background parallaxing

---
+++ Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float cameraFollowPercent = 0.75f;
+
+    private Vector2 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = startPos + ((Vector2)Camera.main.transform.position-startPos) * cameraFollowPercent;
+	}
+}
