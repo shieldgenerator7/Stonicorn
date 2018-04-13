97671877adfb045d6d5633a40455c26eeeaf182b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-04-13 15:30:01 -0400

    Fixed #406 Changed off center teleport preview effects

---
+++ Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour {
+
+    public float turnSpeed = 250;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward * turnSpeed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/TeleportAbility.cs
+++ Assets/Scripts/TeleportAbility.cs
@@ -7,21 +7,11 @@ using System.Collections;
 /// </summary>
 public class TeleportAbility : PlayerAbility
 {//2017-08-07: copied from ForceTeleportAbility
-    public GameObject teleportRangeIndicator;//prefab
-    private TeleportRangeIndicatorUpdater friu;//"force range indicator updater"
-    private GameObject frii;//"force range indicator instance"
     public GameObject futureProjection;//the object that is used to show a preview of the landing spot
-    
+    public GameObject teleportPreviewPointer;//the object that visually points at the future projection
+
     public float maxRange = 3;
     public float maxHoldTime = 1;//how long until the max range is reached
-
-    private PlayerController pc;//reference to the player controller for teleport stuff
-
-    protected override void init()
-    {
-        base.init();
-        pc = GetComponent<PlayerController>();
-    }
 
     public new bool takesGesture()
     {
@@ -43,51 +33,26 @@ public class TeleportAbility : PlayerAbility
         if (finished)
         {
             futureProjection.SetActive(false);
-            Destroy(frii);
-            frii = null;
-            particleController.activateTeleportParticleSystem(false);
-            if (circularProgressBar != null)
-            {
-                circularProgressBar.setPercentage(0);
-            }
+            teleportPreviewPointer.SetActive(false);
         }
         else
         {
+            Vector2 futurePos = playerController.findTeleportablePosition(pos);
+            //Future Projection
             futureProjection.SetActive(true);
             futureProjection.transform.rotation = transform.rotation;
             futureProjection.transform.localScale = transform.localScale;
-            Vector2 futurePos = pc.findTeleportablePosition(pos);
             futureProjection.transform.position = futurePos;
-            if (frii == null)
-            {
-                frii = Instantiate(teleportRangeIndicator);
-                friu = frii.GetComponent<TeleportRangeIndicatorUpdater>();
-                frii.GetComponent<SpriteRenderer>().enabled = false;
-            }
-            frii.transform.position = futurePos;
-            friu.setRange(range);
-            //Particle effects
-            particleController.activateTeleportParticleSystem(true, effectColor, futurePos, range);
-            if (circularProgressBar != null)
-            {
-                circularProgressBar.setPercentage(range / maxRange);
-                circularProgressBar.transform.position = pos;
-            }
+            //Teleport Preview Pointer
+            teleportPreviewPointer.SetActive(true);
+            teleportPreviewPointer.transform.localScale = transform.localScale;
+            teleportPreviewPointer.transform.position = futurePos;
         }
     }
 
     public override void dropHoldGesture()
     {
         futureProjection.SetActive(false);
-        if (frii != null)
-        {
-            Destroy(frii);
-            frii = null;
-        }
-        particleController.activateTeleportParticleSystem(false);
-        if (circularProgressBar != null)
-        {
-            circularProgressBar.setPercentage(0);
-        }
+        teleportPreviewPointer.SetActive(false);
     }
 }
