5cfe191b32a1741565ba0a26000a988855a1cf35
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-04-23 22:38:54 -0400

    Removed Force Charge explode camera requirement

--- Assets/Scripts/ForceTeleportAbility.cs
+++ Assets/Scripts/ForceTeleportAbility.cs
@@ -108,9 +108,7 @@ public class ForceTeleportAbility : PlayerAbility
         Vector2 explodePos = triedPos;
         float range = getRangeFromCharge(currentCharge);
         //If touch position is outside visible blast range, charge;
-        if ((triedPos - oldPos).sqrMagnitude > range * range
-            //or camera is still moving to get into position
-            || playerController.Cam.Displacement.sqrMagnitude > 0.1f)
+        if ((triedPos - oldPos).sqrMagnitude > range * range)
         {
             if (Mathf.Approximately(currentCharge, 0))
             {
