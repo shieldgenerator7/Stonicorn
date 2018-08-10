feafda3193672c1941da51f50112a2b663669d96
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-08-09 20:12:27 -0400

    Background Parallax now keeps background in a predefined area

---
+++ Assets/Editor/BackgroundParallaxEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BackgroundParallax))]
+public class BackgroundParallaxEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        BackgroundParallax bp = (BackgroundParallax)target;
+        if (GUILayout.Button("Set starting point"))
+        {
+            bp.startPos = bp.transform.position;
+        }
+        if (GUILayout.Button("Reset position to starting point"))
+        {
+            bp.transform.position = bp.startPos;
+        }
+        if (GUILayout.Button("Set TOP limit"))
+        {
+            bp.maxBounds.y = bp.transform.position.y;
+        }
+        if (GUILayout.Button("Set BOTTOM limit"))
+        {
+            bp.minBounds.y = bp.transform.position.y;
+        }
+        if (GUILayout.Button("Set LEFT limit"))
+        {
+            bp.minBounds.x = bp.transform.position.x;
+        }
+        if (GUILayout.Button("Set RIGHT limit"))
+        {
+            bp.maxBounds.x = bp.transform.position.x;
+        }
+    }
+
+}
--- Assets/Scripts/BackgroundParallax.cs
+++ Assets/Scripts/BackgroundParallax.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class BackgroundParallax : MonoBehaviour
 {
+    public Vector2 startPos = Vector2.zero;
 
-    [Range(0, 1)]
-    public float cameraFollowPercent = 0.25f;
+    public Vector2 minBounds;//the position will stay inside these bounds
+    public Vector2 maxBounds;
 
-    private Vector2 startPos;
+    public BoxCollider2D cameraContainerCollider;//the collider that has the initial camera containments
+    private Bounds camBounds;
 
     private void Start()
     {
-        startPos = transform.position;
+        if (startPos != (Vector2)transform.position)
+        {
+            if (startPos != Vector2.zero)
+            {
+                transform.position = startPos;
+            }
+            else
+            {
+                startPos = transform.position;
+            }
+        }
+        camBounds = cameraContainerCollider.bounds;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = startPos + (((Vector2)Camera.main.transform.position - startPos) * cameraFollowPercent);
+        Vector2 adjustedPos = Vector2.zero;
+        {
+            float percentX = (Camera.main.transform.position.x - camBounds.min.x) / (camBounds.max.x - camBounds.min.x);
+            adjustedPos.x = Mathf.Clamp(percentX, 0, 1)
+                * (maxBounds.x - minBounds.x) + minBounds.x;
+            adjustedPos.x = Mathf.Clamp(adjustedPos.x, minBounds.x, maxBounds.x);
+        }
+        {
+            float percentY = (Camera.main.transform.position.y - camBounds.min.y) / (camBounds.max.y - camBounds.min.y);
+            adjustedPos.y = Mathf.Clamp(percentY, 0, 1)
+                * (maxBounds.y - minBounds.y) + minBounds.y;
+            adjustedPos.y = Mathf.Clamp(adjustedPos.y, minBounds.y, maxBounds.y);
+        }
+        transform.position = adjustedPos; 
     }
 }
