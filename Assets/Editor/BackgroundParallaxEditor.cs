1f6e9701b5d1ffd4cd943f7f019d4c7625427d7a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-08-09 21:06:16 -0400

    Made "Setup Default Camera Bound Collider" for BackgroundParallax editor

--- Assets/Editor/BackgroundParallaxEditor.cs
+++ Assets/Editor/BackgroundParallaxEditor.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(BackgroundParallax))]
-public class BackgroundParallaxEditor : Editor {
+public class BackgroundParallaxEditor : Editor
+{
 
     public override void OnInspectorGUI()
     {
@@ -33,6 +34,50 @@ public class BackgroundParallaxEditor : Editor {
         if (GUILayout.Button("Set RIGHT limit"))
         {
             bp.maxBounds.x = bp.transform.position.x;
+        }
+        if (GUILayout.Button("Setup Default Camera Bound Collider"))
+        {
+            //Setup bc2d
+            BoxCollider2D bc2d = bp.cameraContainerCollider;
+            if (bc2d == null)
+            {
+                bc2d = bp.GetComponent<BoxCollider2D>();
+                if (bc2d == null)
+                {
+                    bc2d = bp.gameObject.AddComponent<BoxCollider2D>();
+                }
+            }
+            bc2d.isTrigger = true;
+            bp.cameraContainerCollider = bc2d;
+            //Find Bounds of Sprites
+            Bounds bounds = new Bounds(Vector3.zero, Vector3.zero);
+            foreach (Transform t in bp.transform)
+            {
+                foreach (Transform tr in t.gameObject.transform)
+                {
+                    SpriteRenderer sr = tr.GetComponent<SpriteRenderer>();
+                    if (sr != null)
+                    {
+                        if (bounds.size == Vector3.zero)
+                        {
+                            bounds = sr.bounds;
+                        }
+                        else
+                        {
+                            bounds.min = Vector3.Min(bounds.min, sr.bounds.min);
+                            bounds.max = Vector3.Max(bounds.max, sr.bounds.max);
+                        }
+                    }
+                }
+            }
+            //Set bc2d offset
+            bc2d.offset = bounds.center - bp.transform.position;
+            //Set bc2d size
+            bounds.size += new Vector3(
+                bp.maxBounds.x - bp.minBounds.x,
+                bp.maxBounds.y - bp.minBounds.y
+                );
+            bc2d.size = bounds.size;
         }
     }
 
