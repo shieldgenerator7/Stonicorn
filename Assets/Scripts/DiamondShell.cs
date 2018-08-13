cb49c7d70cf21b4fe9d852ccc876c7f8bf3b5a99
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-08-13 00:51:36 -0400

    DiamondShells can now move both left and right

--- Assets/Scripts/DiamondShell.cs
+++ Assets/Scripts/DiamondShell.cs
@@ -11,6 +11,7 @@ public class DiamondShell : MonoBehaviour
 
     //Runtime vars
     private float speed = 0;//current speed
+    private float direction = 1;//-1 for left, 1 for right
 
     //Components
     private Rigidbody2D rb2d;
@@ -25,34 +26,32 @@ public class DiamondShell : MonoBehaviour
     void Update()
     {
         //Check to see if there's any stones in sight
-        RaycastHit2D[] rch2ds = Physics2D.RaycastAll(transform.position, transform.right, sightRange);
-        Debug.DrawLine(transform.position, transform.position + transform.right * sightRange, Color.blue);
-        bool accelerating = false;
-        foreach (RaycastHit2D rch2d in rch2ds)
+        float distLeft = checkFoodInDirection(-1.0f);
+        float distRight = checkFoodInDirection(1.0f);
+        //If any stones in range
+        if (distLeft > 0 || distRight > 0)
         {
-            if (rch2d && rch2d.collider.gameObject != gameObject)
+            if (distLeft > distRight)
             {
-                HardMaterial hm = rch2d.collider.gameObject.GetComponent<HardMaterial>();
-                if (hm)
-                {
-                    Debug.DrawLine(transform.position, rch2d.point, Color.red);
-                    Debug.Log("DiamondShell (" + gameObject.name + ") sees object: " + rch2d.collider.gameObject.name);
-                    speed += accelerationPerSecond * Time.deltaTime;
-                    accelerating = true;
-                }
+                direction = -1;
             }
-        }
-        //Slow down if cant find anything
-        if (!accelerating)
-        {
-            if (speed > 0)
+            else
             {
-                speed -= accelerationPerSecond * Time.deltaTime;
-                speed = Mathf.Max(speed, 0);
+                direction = 1;
+            }
+            //Increase speed in that direction
+            speed += accelerationPerSecond * direction * Time.deltaTime;
+        }
+        else
+        {
+            //Otherwise slow down
+            if (speed != 0)
+            {
+                speed = Mathf.MoveTowards(speed, 0, accelerationPerSecond * Time.deltaTime);
             }
         }
         //If moving, addforce to keep moving
-        if (speed > 0)
+        if (speed != 0)
         {
             rb2d.AddForce(transform.right * rb2d.mass * speed);
         }
@@ -68,5 +67,33 @@ public class DiamondShell : MonoBehaviour
             //Stop
             speed = 0;
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="direction">-1 for left, 1 for right</param>
+    /// <returns></returns>
+    float checkFoodInDirection(float direction)
+    {
+        RaycastHit2D[] rch2ds = Physics2D.RaycastAll(transform.position, transform.right * direction, sightRange);
+        Debug.DrawLine(transform.position, transform.position + transform.right * Mathf.Sign(direction) * sightRange, Color.blue);
+
+        foreach (RaycastHit2D rch2d in rch2ds)
+        {
+            if (rch2d && rch2d.collider.gameObject != gameObject)
+            {
+                HardMaterial hm = rch2d.collider.gameObject.GetComponent<HardMaterial>();
+                if (hm)
+                {
+                    Debug.DrawLine(transform.position, rch2d.point, Color.red);
+                    Debug.Log("DiamondShell (" + gameObject.name + ") sees object: " + rch2d.collider.gameObject.name);
+                    return 
+                        (sightRange - rch2d.distance)//the closer the object is, the higher this number will be
+                        + hm.getIntegrity();//the healthier this object is, the higher this number will be
+                }
+            }
+        }
+        return 0;
     }
 }
