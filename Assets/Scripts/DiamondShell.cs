75f5f2fa183dec75e34f12387808c823bb661d71
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-08-14 21:11:16 -0400

    Updates to DiamondShell AI

--- Assets/Scripts/DiamondShell.cs
+++ Assets/Scripts/DiamondShell.cs
@@ -10,14 +10,22 @@ public class DiamondShell : MonoBehaviour
     //Settings
     public float accelerationPerSecond = 2.0f;//how fast the diamondshell can accelerate each second
     public float initialSpeed = 2.0f;//start up speed independent of acceleration
+    public float maxSpeed = 10.0f;//max speed
+    public float accelDuration = 2.0f;//how long it takes to get to max speed
     public float sightRange = 10.0f;//how far it can see from its center
     public string food = "stone";
     [Range(0, 1)]
     public float onDamageHealPercent = 0.2f;//what percent of damage it does to others it heals itself
+    public float quickTurnDuration = 1.0f;//how long quick turns last
 
     //Runtime vars
     private float speed = 0;//current speed
     private float direction = 0;//-1 for left, 1 for right, 0 for stopped
+    public float accelerationPerSecond = 0;//how fast the diamondshell can accelerate each second
+    public float speed = 0;//current speed
+    public float direction = 0;//-1 for left, 1 for right, 0 for stopped
+    public float quickTurnStartTime = 0;
+    public float quickTurnDirection = 0;//-1 for left, 1 for right, 0 for no quickTurn
 
     //Components
     private Rigidbody2D rb2d;
@@ -29,10 +37,12 @@ public class DiamondShell : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         hm = GetComponent<HardMaterial>();
         hm.hardCollision += eatDamage;
+        accelerationPerSecond = maxSpeed / accelDuration;
     }
 
     // Update is called once per frame
     void Update()
+    void FixedUpdate()
     {
         //Check to see if there's any stones in sight
         float distLeft = checkFoodInDirection(-1.0f);
@@ -53,13 +63,18 @@ public class DiamondShell : MonoBehaviour
             {
                 //Increase speed in that direction
                 speed += accelerationPerSecond * Time.deltaTime;
+                speed += accelerationPerSecond * Time.fixedDeltaTime;
+                speed = Mathf.Min(speed, maxSpeed);
             }
             else
             {
                 if (speed <= initialSpeed)
+                speed -= accelerationPerSecond * 0.5f * Time.fixedDeltaTime;
+                if (speed <= 0)
                 {
                     //Jump ahead to a speed
                     speed = initialSpeed;
+                    speed = 0;
                 }
                 else
                 {
@@ -75,6 +90,7 @@ public class DiamondShell : MonoBehaviour
             if (speed > 0)
             {
                 speed -= accelerationPerSecond * Time.deltaTime;
+                speed -= accelerationPerSecond * Time.fixedDeltaTime;
                 speed = Mathf.Max(speed, 0);
             }
             else
@@ -92,6 +108,7 @@ public class DiamondShell : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         float angle = Vector2.Angle(transform.right, collision.contacts[0].point - (Vector2)transform.position);
+        float angle = Vector2.Angle(transform.right*direction, collision.contacts[0].point - (Vector2)transform.position);
         Debug.Log("DiamondShell (" + gameObject.name + ") hit something: " + collision.collider.gameObject.name + ", angle: " + angle);
         //If crashed into something in the direction of travel, 
         if (angle < 10)
@@ -131,7 +148,7 @@ public class DiamondShell : MonoBehaviour
                 {
                     Debug.DrawLine(transform.position, rch2d.point, Color.red);
                     Debug.Log("DiamondShell (" + gameObject.name + ") sees object: " + rch2d.collider.gameObject.name);
-                    return 
+                    return
                         (sightRange - rch2d.distance)//the closer the object is, the higher this number will be
                         + hm.getIntegrity();//the healthier this object is, the higher this number will be
                 }
