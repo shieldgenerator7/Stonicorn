0ae89078e93524c8a13b60361ee5bd188ee9fb83
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-22 19:40:44 -0500

    Fixed extruded vertex bug when crossing the loop threshold on the stencil

--- Assets/Editor/PolygonCollider2DWorkerEditor.cs
+++ Assets/Editor/PolygonCollider2DWorkerEditor.cs
@@ -218,9 +218,9 @@ public class PolygonCollider2DWorkerEditor : Editor
                     //Get the new path data
                     newPath[0] = intersectionData[dataStart].intersectionPoint;
                     int writeIndex = 1;
-                    for (int i = startIndex; i > endIndex; i--, writeIndex++)
+                    for (int i = startIndex + stencilCount; i > endIndex; i--, writeIndex++)
                     {
-                        newPath[writeIndex] = stencilPoints[i];
+                        newPath[writeIndex] = stencilPoints[(i + stencilCount) % stencilCount];
                     }
                     newPath[newPath.Length - 1] = intersectionData[dataEnd].intersectionPoint;
                     //Replace vein with stencil path
