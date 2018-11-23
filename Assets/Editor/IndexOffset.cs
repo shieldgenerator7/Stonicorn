15ddbfecff46f3430d773c50f08b62da4b315aa7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-22 22:15:06 -0500

    Added IndexOffset class

---
+++ Assets/Editor/IndexOffset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to keep track off the target's line segments while they're being moved around
+/// It basically keeps track of changes made to the points array
+/// </summary>
+public class IndexOffset
+{
+    int index;//the index where things were inserted or deleted
+    int count;//the amount of points inserted or deleted
+    bool inserted = true;//true = points inserted, false = points deleted
+
+    public IndexOffset(int index, int count)
+    {
+        this.index = index;
+        this.count = count;
+    }
+
+    public static int getNewIndex(List<IndexOffset> offsets, int index, int originalCount)
+    {
+        foreach (IndexOffset io in offsets)
+        {
+            if (io.inserted)
+            {
+                if (io.index < index)
+                {
+                    index += io.count;
+                }
+                originalCount += io.count;
+            }
+            else
+            {
+                if (io.index < index)
+                {
+                    index -= io.count;
+                }
+                else if ((io.index + io.count - 1) % originalCount < index)
+                {
+                    index -= (io.index + io.count - 1) % originalCount;
+                }
+                originalCount -= io.count;
+            }
+        }
+        return index;
+    }
+}
