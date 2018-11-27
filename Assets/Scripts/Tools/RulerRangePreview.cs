9b0d1eb5d77199611179f25b2b7a8bb65321cca9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-11-27 15:19:05 -0500

    Added a teleport preview to the range tool

---
+++ Assets/Scripts/Tools/RulerRangePreview.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class RulerRangePreview : MonoBehaviour
+{
+
+    [Tooltip("How far Merky can teleport")]
+    public float range = 3;
+
+    [Tooltip("True to turn on while the ruler isn't moving")]
+    public bool active = true;
+
+    public RulerDisplayer parentRuler;
+
+    private void OnDrawGizmos()
+    {
+        if (active)
+        {
+            if (!parentRuler.active)
+            {
+                Vector2 currentMousePos = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition).origin;
+                Vector2 direction = (currentMousePos - (Vector2)parentRuler.transform.position);
+                if (direction.sqrMagnitude > range * range)
+                {
+                    direction = direction.normalized * range;
+                }
+                transform.position = direction + (Vector2)parentRuler.transform.position;
+            }
+        }
+    }
+}
