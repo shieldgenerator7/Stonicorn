9b91aed9cc38b57d49ea86cb1d647af2e763ed81
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-06 23:49:36 -0500

    Camera move tutorial now starts at target and moves towards camera

--- Assets/Scripts/Effects/DirectedMovement.cs
+++ Assets/Scripts/Effects/DirectedMovement.cs
@@ -10,8 +10,13 @@ public class DirectedMovement : SimpleMovement
     public string actorTag = "MainCamera";//the tag on the actor to identify it
     public string actorName = "Main Camera";//the name of the actor to identify it
     private GameObject actor;//the game object that this object follows
+    public bool startFromActor = false;//true to start from the actor, false to start from the target
+    public bool pointAway = false;//true to point from anchor away from the goal, false to point towards the goal
 
-    private Vector2 prevPos;
+    private Vector2 prevPosAnchor;
+    private Vector2 prevPosGoal;
+    private GameObject anchorObject;
+    private GameObject goalObject;
 
     // Use this for initialization
     protected override void Start()
@@ -25,24 +30,38 @@ public class DirectedMovement : SimpleMovement
                 break;
             }
         }
-        Debug.Log("Actor name: " + actor.name);
+        //Set anchor object
+        anchorObject = (startFromActor) ? actor : target;
+        //Set goal object
+        goalObject = (!startFromActor) ? actor : target;
         //Make sure direction is valid
         if (direction == Vector2.zero)
         {
             direction = Vector2.one;
         }
         //Set initial movement vector
-        setMovement((Vector2)actor.transform.position, (Vector2)actor.transform.position - (Vector2)target.transform.position, minDistance, maxDistance, false, true);
+        setMovement((Vector2)anchorObject.transform.position, getDirection(), minDistance, maxDistance, false, true);
     }
 
     // Update is called once per frame
     protected override void Update()
     {
-        if ((Vector2)actor.transform.position != prevPos)
+        if ((Vector2)anchorObject.transform.position != prevPosAnchor
+            || (Vector2)goalObject.transform.position != prevPosGoal)
         {
-            prevPos = actor.transform.position;
-            setMovement(actor.transform.position, actor.transform.position - target.transform.position, minDistance, maxDistance);
+            prevPosAnchor = anchorObject.transform.position;
+            prevPosGoal = goalObject.transform.position;
+            setMovement(anchorObject.transform.position, getDirection(), minDistance, maxDistance);
         }
         base.Update();
+    }
+
+    Vector2 getDirection()
+    {
+        return (
+            (Vector2)goalObject.transform.position - (Vector2)anchorObject.transform.position
+            )
+            * ((pointAway) ? -1 : 1)
+            ;
     }
 }
