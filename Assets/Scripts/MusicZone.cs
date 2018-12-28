f54f01edad61351d824c285e2535ea9d4e168519
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-28 02:32:09 -0500

    Refactored MusicManager to get ready for sliding control

--- Assets/Scripts/MusicZone.cs
+++ Assets/Scripts/MusicZone.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicZone : MonoBehaviour {
+public class MusicZone : MonoBehaviour
+{
 
     private AudioSource music;
     private MusicManager musicManager;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         music = GetComponent<AudioSource>();
         music.volume = 0;
         musicManager = FindObjectOfType<MusicManager>();
-	}
-    
+    }
+
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == GameManager.playerTag)
+        if (coll.gameObject.CompareTag(GameManager.playerTag))
         {
             musicManager.setCurrentSong(music);
         }
