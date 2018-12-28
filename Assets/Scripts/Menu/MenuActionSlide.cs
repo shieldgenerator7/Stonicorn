eb97de43c0cba2829475a754a4b1ba5ff1ee5a3e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-28 02:33:25 -0500

    Menu Music slider now controls music volume

---
+++ Assets/Scripts/Menu/MenuActionSlide.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MenuActionSlide : MonoBehaviour
+{
+    public abstract void valueAdjusted(float value);
+
+    public abstract float getCurrentValue();
+}
