0857eb3088ae2f9bfa9a694e122c6a4635d60c02
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-12-29 05:32:58 -0500

    Sound slider controls sound volume

---
+++ Assets/Scripts/Menu/ButtonSpecific/MenuActionSlideSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActionSlideSound : MenuActionSlide
+{
+    public override void valueAdjusted(float value)
+    {
+        FindObjectOfType<SoundManager>().Volume = value;
+    }
+
+    public override float getCurrentValue()
+    {
+        return FindObjectOfType<SoundManager>().Volume;
+    }
+}
