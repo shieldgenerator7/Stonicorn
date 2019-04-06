b528b1a4dc54545253e07050ce83d3def608cb83
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-04-06 16:53:11 -0400

    Replaced ForceTeleport with ForceBoost in the PlayerScene

---
+++ Assets/Scripts/MilestoneActivatorForceBoost.cs
@@ -0,0 +1,9 @@
+﻿
+
+public class MilestoneActivatorForceBoost : MilestoneActivator
+{//2019-04-06: copied from MilestoneActivatorForceTeleport
+    public override void activateEffect()
+    {
+        Managers.Player.GetComponent<ForceBoostAbility>().enabled = true;
+    }
+}
