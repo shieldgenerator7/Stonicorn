63a54d270a64be6b01f0691502b43bd61442ce69
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-29 17:59:43 -0400

    Added OrthoToPerspective Converter

---
+++ Assets/Editor/OrthoToPerspectiveConverterEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(OrthoToPerspectiveConverter))]
+public class OrthoToPerspectiveConverterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        OrthoToPerspectiveConverter otpc = ((OrthoToPerspectiveConverter)target);
+        if (GUILayout.Button("Select All"))
+        {
+            otpc.selectAll();
+        }
+        if (GUILayout.Button("Convert"))
+        {
+            otpc.convert();
+        }
+        if (GUILayout.Button("Reset Settings to Default"))
+        {
+            otpc.settingsToDefault();
+        }
+    }
+}
