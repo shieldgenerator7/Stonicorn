7a955c6102bd14de053438b370ec0c5e3ea8031d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-09-02 00:06:58 -0400

    Added MilestoneActivatorAbility
    so that I don't have to make a new class everytime I want an ability activator that just activates things

---
+++ Assets/Scripts/MilestoneActivatorAbility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class MilestoneActivatorAbility : MilestoneActivator {
+
+    public string abilityTypeName;
+
+    public override void activateEffect()
+    {
+        ((PlayerAbility)Managers.Player.GetComponent(abilityTypeName)).enabled = true;
+    }
+}
