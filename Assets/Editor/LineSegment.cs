3cccf7c96b86770ac14810b9d987c596d0778e28
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-09-03 22:22:17 -0400

    Refactored polygon cutting tool to allow for future other types

--- Assets/Editor/LineSegment.cs
+++ Assets/Editor/LineSegment.cs
@@ -62,6 +62,96 @@ public class LineSegment
         }
         private set { }
     }
+
+    public bool Intersects(LineSegment l2, ref Vector2 intersection)
+    {
+        return LineIntersection(this.startPos, this.endPos, l2.startPos, l2.endPos, ref intersection);
+    }
+    /// <summary>
+    /// Returns true if the two lines intersect
+    /// 2018-11-21: copied from a post by Minassian: https://forum.unity.com/threads/line-intersection.17384/
+    /// </summary>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <param name="p3"></param>
+    /// <param name="p4"></param>
+    /// <param name="intersection"></param>
+    /// <returns></returns>
+    public static bool LineIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, ref Vector2 intersection)
+    {
+        float Ax, Bx, Cx, Ay, By, Cy, d, e, f, num;
+        float x1lo, x1hi, y1lo, y1hi;
+        Ax = p2.x - p1.x;
+        Bx = p3.x - p4.x;
+        // X bound box test/
+        if (Ax < 0)
+        {
+            x1lo = p2.x; x1hi = p1.x;
+        }
+        else
+        {
+            x1hi = p2.x; x1lo = p1.x;
+        }
+        if (Bx > 0)
+        {
+            if (x1hi < p4.x || p3.x < x1lo) return false;
+        }
+        else
+        {
+            if (x1hi < p3.x || p4.x < x1lo) return false;
+        }
+        Ay = p2.y - p1.y;
+        By = p3.y - p4.y;
+        // Y bound box test//
+        if (Ay < 0)
+        {
+            y1lo = p2.y; y1hi = p1.y;
+        }
+        else
+        {
+            y1hi = p2.y; y1lo = p1.y;
+        }
+        if (By > 0)
+        {
+            if (y1hi < p4.y || p3.y < y1lo) return false;
+        }
+        else
+        {
+            if (y1hi < p3.y || p4.y < y1lo) return false;
+        }
+        Cx = p1.x - p3.x;
+        Cy = p1.y - p3.y;
+        d = By * Cx - Bx * Cy;  // alpha numerator//
+        f = Ay * Bx - Ax * By;  // both denominator//
+                                // alpha tests//
+        if (f > 0)
+        {
+            if (d < 0 || d > f) return false;
+        }
+        else
+        {
+            if (d > 0 || d < f) return false;
+        }
+        e = Ax * Cy - Ay * Cx;  // beta numerator//
+                                // beta tests //
+        if (f > 0)
+        {
+            if (e < 0 || e > f) return false;
+        }
+        else
+        {
+            if (e > 0 || e < f) return false;
+        }
+        // check if they are parallel
+        if (f == 0) return false;
+        // compute intersection coordinates //
+        num = d * Ax; // numerator //
+        intersection.x = p1.x + num / f;
+        num = d * Ay;
+        intersection.y = p1.y + num / f;
+        return true;
+    }
+
     public override string ToString()
     {
         return "Line Segment: " + startPos + " -> " + endPos
---
+++ Assets/Editor/Shape.cs
@@ -0,0 +1,347 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape
+{
+    public Transform transform { get; private set; }
+    public Vector2 position { get; private set; }
+    public Vector2 scale { get; private set; }
+    public Bounds bounds { get; private set; }
+
+    private List<Vector2> points;
+    private bool isInLocalSpace = true;
+
+    public List<Vector2> LocalPoints
+    {
+        get
+        {
+            if (!isInLocalSpace)
+            {
+                isInLocalSpace = true;
+                convertPathToLocalSpace();
+            }
+            return points;
+        }
+    }
+
+    public List<Vector2> GlobalPoints
+    {
+        get
+        {
+            if (isInLocalSpace)
+            {
+                isInLocalSpace = false;
+                convertPathToWorldSpace();
+            }
+            return points;
+        }
+    }
+
+    public List<Shape> childrenShapes { get; private set; } = new List<Shape>();
+
+    private PolygonCollider2D pc2d = null;
+
+    public Shape(PolygonCollider2D pc2d)
+    {
+        this.pc2d = pc2d;
+        this.transform = pc2d.transform;
+        this.position = pc2d.transform.position;
+        this.scale = pc2d.transform.localScale;
+        this.bounds = pc2d.bounds;
+        this.points = new List<Vector2>(pc2d.GetPath(0));
+    }
+
+    public void cutShape(Shape stencil, bool splitFurther = true)
+    {
+        //Show paths (for debugging)
+        //showPath(points);
+        //showPath(stencilPoints);
+
+        //Gather overlap info
+        List<IntersectionData> intersectionData = new List<IntersectionData>();
+        for (int i = 0; i < GlobalPoints.Count; i++)
+        {
+            int i2 = (i + 1) % GlobalPoints.Count;
+
+            //Line Checking
+            LineSegment targetLine = new LineSegment(GlobalPoints, i);
+            //Check to see if the bounds overlap
+            if (stencil.bounds.Intersects(targetLine.Bounds))
+            {
+                bool startInStencil = stencil.OverlapPoint(targetLine.startPos);
+                bool endInStencil = stencil.OverlapPoint(targetLine.endPos);
+                //Check which stencil edges intersect the line segment
+                bool intersectsSegment = false;
+                for (int j = 0; j < stencil.GlobalPoints.Count; j++)
+                {
+                    LineSegment stencilLine = new LineSegment(stencil.GlobalPoints, j);
+                    Vector2 intersection = Vector2.zero;
+                    bool intersects = targetLine.Intersects(stencilLine, ref intersection);
+                    //If it intersects,
+                    if (intersects)
+                    {
+                        //Record a data point
+                        intersectsSegment = true;
+                        float distanceToPoint = (intersection - targetLine.startPos).magnitude;
+                        IntersectionData interdata = new IntersectionData(intersection, i, j, intersects, startInStencil, endInStencil, distanceToPoint);
+                        intersectionData.Add(interdata);
+                    }
+                }
+                //If no line segment intersections were found,
+                if (!intersectsSegment)
+                {
+                    //but one or more end points are in the stencil,
+                    if (startInStencil || endInStencil)
+                    {
+                        //Make an intersection data point anyway, with slightly different arguments
+                        IntersectionData interdata = new IntersectionData(Vector2.zero, i, -1, IntersectionData.IntersectionType.INSIDE);
+                        intersectionData.Add(interdata);
+                    }
+                }
+                //else,
+                else
+                {
+                    //do nothing because the bounds lied about the line segment and stencil colliding
+                    //don't worry, it's a known thing that can happen:
+                    //bounds checking is quick but liable to give false positives
+                }
+            }
+
+        }
+
+        //
+        // Refine intersection data entries
+        //
+
+        //Sort the data entries
+        intersectionData.Sort(new IntersectionData.IntersectionDataComparer());
+
+        //Set the intersection type of the data
+        int side = 0;//0 =not set, 1 =inside, -1 =outside
+        foreach (IntersectionData interdata in intersectionData)
+        {
+            if (side == 0)
+            {
+                side = (interdata.startsInStencil) ? 1 : -1;
+            }
+            if (interdata.segmentIntersection)
+            {
+                side *= -1;
+                interdata.type = (side > 0) ? IntersectionData.IntersectionType.ENTER : IntersectionData.IntersectionType.EXIT;
+            }
+            else
+            {
+                interdata.type = (side > 0) ? IntersectionData.IntersectionType.INSIDE : IntersectionData.IntersectionType.OUTSIDE;
+            }
+        }
+        IntersectionData.printDataList(intersectionData, GlobalPoints);
+
+        //
+        //Start cutting
+        //
+
+        //Replace line segments inside the stencil
+        int dataCount = intersectionData.Count;
+        //Search for start of vein of changes
+        List<Vein> veins = new List<Vein>();
+        //only need to go through the loop once,
+        //because the veins will find their own end points:
+        //here we just need to find the start of each vein
+        for (int iData = 0; iData < dataCount; iData++)
+        {
+            IntersectionData interdata = intersectionData[iData];
+            //if this segment enters the stencil at this data point,
+            if (interdata.type == IntersectionData.IntersectionType.ENTER)
+            {
+                //then it's a vein start
+                Vein vein = new Vein(iData, interdata, intersectionData);
+                veins.Add(vein);
+            }
+        }
+        //Process found veins
+        if (veins.Count == 1)
+        {
+            Vector2[] newPath = veins[0].getStencilPath(stencil.GlobalPoints);
+            //Replace vein with stencil path
+            int removeCount = veins[0].getRemoveCount(GlobalPoints.Count);
+            replacePoints(newPath, veins[0].VeinStart + 1, removeCount);
+        }
+        else
+        {
+            //Process all the veins
+            IndexOffset.IndexOffsetContainer offsets = new IndexOffset.IndexOffsetContainer(GlobalPoints.Count);
+            for (int i = 0; i < veins.Count; i++)
+            {
+                Vein vein = veins[i];
+                //Update vein with new offsets
+                vein.updateIndexes(offsets);
+                //Check next vein
+                bool slices = false;
+                if (i < veins.Count - 1)
+                {
+                    Vein vein2 = veins[i + 1];
+                    vein.updateIndexes(offsets);
+                    slices = vein.formsSlice(vein2, stencil.GlobalPoints.Count);
+                    Debug.Log("slices: " + slices);
+                    if (slices)
+                    {
+                        vein.slice(vein2);
+                        if (splitFurther)
+                        {
+                            if (this.pc2d)
+                            {
+                                //make a new collider to make the new piece
+                                PolygonCollider2D pc2dNew = GameObject.Instantiate(pc2d.gameObject)
+                                    .GetComponent<PolygonCollider2D>();
+                                Shape newShape = new Shape(pc2dNew);
+                                newShape.rotatePoints(vein2.VeinStart);
+                                newShape.finalize();
+                                childrenShapes.Add(newShape);
+                                pc2dNew.transform.parent = pc2d.transform.parent;
+                                pc2dNew.transform.position = pc2d.transform.position;
+                                newShape.cutShape(stencil, false);
+                            }
+                        }
+                        //skip the next vein
+                        i++;
+                    }
+                }
+                if (true || !slices)
+                {
+                    //Replace vein with stencil path
+                    Vector2[] newPath = vein.getStencilPath(stencil.GlobalPoints);
+                    int removeCount = vein.getRemoveCount(GlobalPoints.Count);
+                    replacePoints(newPath, vein.VeinStart + 1, removeCount);
+                    //Add offset to the collection
+                    IndexOffset offset = new IndexOffset(vein.VeinStart, newPath.Length - removeCount);
+                    offsets.Add(offset);
+                }
+            }
+        }
+
+        //
+        // Finish up
+        //
+        finalize();
+    }
+
+    public bool OverlapPoint(Vector2 point)
+    {
+        if (this.pc2d)
+        {
+            return this.pc2d.OverlapPoint(point);
+        }
+        PolygonCollider2D pc2d = new PolygonCollider2D();
+        pc2d.SetPath(0, LocalPoints);
+        return pc2d.OverlapPoint(point);
+    }
+
+    //
+    // Private methods
+    //
+
+    private void finalize()
+    {
+        if (this.pc2d)
+        {
+            this.pc2d.SetPath(0, LocalPoints.ToArray());
+        }
+    }
+
+    private void convertPathToWorldSpace()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector2 v = points[i];
+            v = transform.TransformPoint(v);
+            points[i] = v;
+        }
+        isInLocalSpace = false;
+    }
+    static void convertPathToWorldSpace(ref List<Vector2> points, Vector2 center, Vector2 scale)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector2 v = points[i];
+            v = (v * scale) + center;
+            points[i] = v;
+        }
+    }
+    private void convertPathToLocalSpace()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector2 v = points[i];
+            v = transform.InverseTransformPoint(v);
+            points[i] = v;
+        }
+        isInLocalSpace = true;
+    }
+    static void convertPathToLocalSpace(ref List<Vector2> points, Vector2 center, Vector2 scale)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Vector2 v = points[i];
+            v = (v - center) / scale;
+            points[i] = v;
+        }
+    }
+
+    /// <summary>
+    /// Replaces vectors in its Global Points list for the ones in the newVectors array at the specified index
+    /// </summary>
+    /// <param name="newVectors"></param>
+    /// <param name="index"></param>
+    /// <param name="removeCount"></param>
+    private void replacePoints(Vector2[] newVectors, int index, int removeCount)
+    {
+        insertPoints(newVectors, index);
+        index += newVectors.Length;
+        removePoints(index, removeCount);
+    }
+    /// <summary>
+    /// Inserts the points given in the vectors array into its Global Points list
+    /// </summary>
+    /// <param name="vectors"></param>
+    /// <param name="index"></param>
+    private void insertPoints(Vector2[] vectors, int index)
+    {
+        GlobalPoints.InsertRange(index, vectors);
+    }
+    /// <summary>
+    /// Removes the quantified vectors in its Global Points list at the given index
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="count"></param>
+    private void removePoints(int index, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (index >= points.Count)
+            {
+                index = 0;
+            }
+            GlobalPoints.RemoveAt(index);
+        }
+    }
+
+    /// <summary>
+    /// Rotates the points forward or backward in the array to change the anchor point
+    /// </summary>
+    /// <param name="points"></param>
+    /// <param name="offset"></param>
+    private void rotatePoints(int newAnchor)
+    {
+        List<Vector2> originalPoints = new List<Vector2>(GlobalPoints.ToArray());
+        int count = GlobalPoints.Count;
+        for (int i = 0; i < GlobalPoints.Count; i++)
+        {
+            Vector2 v = originalPoints[(i + newAnchor) % count];
+            GlobalPoints[i] = v;
+        }
+    }
+
+    public static implicit operator PolygonCollider2D(Shape shape)
+        => shape.pc2d;
+}
