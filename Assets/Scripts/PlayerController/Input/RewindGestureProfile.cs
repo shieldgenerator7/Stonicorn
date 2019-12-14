0958570528d45bf25fb742316cb5334e20f59b91
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-12-14 09:42:12 -0500

    Game pauses when zooming out

--- Assets/Scripts/PlayerController/Input/RewindGestureProfile.cs
+++ Assets/Scripts/PlayerController/Input/RewindGestureProfile.cs
@@ -5,10 +5,16 @@ public class RewindGestureProfile : GestureProfile
 {
     public override void activate()
     {
+        //Show Previous Teleport Points
         Managers.Game.showPlayerGhosts(true);
+        //Pause game
+        Time.timeScale = 0;
     }
     public override void deactivate()
     {
+        //Unpause
+        Time.timeScale = 1;
+        //Hide Previous Teleport Points
         Managers.Game.showPlayerGhosts(false);
     }
     public override void processTapGesture(Vector3 curMPWorld)
