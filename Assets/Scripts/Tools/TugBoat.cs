6c4857f1631c6316a154602596c0f602bc8f6db2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-04-15 09:31:36 -0400

    Create TugBoat tool*
    *TugBoat moves from its location to the target position, pulling everything with it in that direction

---
+++ Assets/Scripts/Tools/TugBoat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TugBoat : MonoBehaviour
+{
+    public Vector3 targetPosition;
+
+    public void move()
+    {
+        Vector3 direction = targetPosition - transform.position;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                foreach (GameObject go in s.GetRootGameObjects())
+                {
+                    go.transform.position += direction;
+                }
+                EditorSceneManager.MarkSceneDirty(s);
+            }
+        }
+    }
+}
