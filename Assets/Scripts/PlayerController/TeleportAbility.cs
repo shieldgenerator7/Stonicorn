e40224e54ba71bb3739983fe68dec53ac398cf23
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-11-18 07:14:01 -0500

    Add Flashlight ability

--- Assets/Scripts/PlayerController/TeleportAbility.cs
+++ Assets/Scripts/PlayerController/TeleportAbility.cs
@@ -7,8 +7,39 @@ using System.Collections;
 /// </summary>
 public class TeleportAbility : PlayerAbility
 {//2017-08-07: copied from ForceTeleportAbility
+    [Header("Future Projection")]
     public GameObject futureProjection;//the object that is used to show a preview of the landing spot
     public GameObject teleportPreviewPointer;//the object that visually points at the future projection
+    [Header("Flashlight")]
+    public float maxPullBackDistance = 6;
+    public GameObject flashlightPrefab;//prefab
+    private GameObject flashlight;
+    private bool flashlightOn = false;
+    private Vector2 flashlightDirection;
+    public Vector2 FlashlightDirection
+    {
+        get => flashlightDirection;
+        private set
+        {
+            flashlightDirection = value;
+            if (flashlightDirection.magnitude > maxPullBackDistance)
+            {
+                flashlightDirection = flashlightDirection.normalized * maxPullBackDistance;
+            }
+        }
+    }
+
+    protected override void init()
+    {
+        base.init();
+        playerController.onDragGesture += processDrag;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        playerController.onDragGesture -= processDrag;
+    }
 
     public override void processHoldGesture(Vector2 pos, float holdTime, bool finished)
     {
@@ -35,5 +66,36 @@ public class TeleportAbility : PlayerAbility
     {
         futureProjection.SetActive(false);
         teleportPreviewPointer.SetActive(false);
+    }
+
+    public void processDrag(Vector2 oldPos, Vector2 newPos, bool finished)
+    {
+        flashlightOn = !finished;
+        FlashlightDirection = (Vector2)playerController.transform.position - newPos;
+        updateFlashlightVisuals();
+    }
+
+    void updateFlashlightVisuals()
+    {
+        if (flashlightOn)
+        {
+            if (this.flashlight == null)
+            {
+                this.flashlight = Instantiate(flashlightPrefab);
+                this.flashlight.transform.parent = transform;
+                this.flashlight.transform.localPosition = Vector2.zero;
+            }
+            flashlight.SetActive(true);
+            flashlight.transform.up = flashlightDirection;
+            flashlight.transform.localScale = new Vector3(
+                1,
+                flashlightDirection.magnitude,
+                1
+                );
+        }
+        else
+        {
+            flashlight?.SetActive(false);
+        }
     }
 }
