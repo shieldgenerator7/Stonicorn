9a5de93868763382d66b3fda20fff161b95eea5b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-11-19 02:56:53 -0500

    Create Crab prefab

---
+++ Assets/Scripts/EnemyAI/CrabController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabController : Hazard
+{
+    public Color shellColor = Color.white;
+
+    public override bool Hazardous => false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        new List<SpriteRenderer>(GetComponentsInChildren<SpriteRenderer>())
+            .ForEach(sr => sr.color = shellColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
