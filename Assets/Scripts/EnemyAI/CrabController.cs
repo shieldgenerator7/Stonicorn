d2aba371f2fd21125660da2ecbf9d39869d99a9f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-11-19 03:57:00 -0500

    Add ColorInitializer

--- Assets/Scripts/EnemyAI/CrabController.cs
+++ Assets/Scripts/EnemyAI/CrabController.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class CrabController : Hazard
 {
-    public Color shellColor = Color.white;
-    public Color eyeColor = Color.white;
-    public List<SpriteRenderer> eyeSRs;
+    public float moveSpeed = 1;
 
     public override bool Hazardous => false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Shell colors
-        new List<SpriteRenderer>(GetComponentsInChildren<SpriteRenderer>())
-            .ForEach(sr => sr.color = shellColor);
-        //Eye colors
-        eyeSRs
-            .ForEach(sr => sr.color = eyeColor);
+
     }
 
     // Update is called once per frame
