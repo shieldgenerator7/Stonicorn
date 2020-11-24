43e63df893575df5a7f626e348ec599ae11ab2e6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-11-23 21:21:14 -0500

    PrefabSwitcher auto-selects whatever you select

--- Assets/Editor/PrefabSwitcherEditor.cs
+++ Assets/Editor/PrefabSwitcherEditor.cs
@@ -7,18 +7,32 @@ using UnityEngine;
 [CustomEditor(typeof(PrefabSwitcher))]
 public class PrefabSwitcherEditor : Editor
 {
+    PrefabSwitcher ps;
+
+    void OnEnable()
+    {
+        ps = (PrefabSwitcher)target;
+        Selection.selectionChanged += delegate ()
+        {
+            ps?.setOldObjs(new List<GameObject>(
+                Selection.GetFiltered<GameObject>(SelectionMode.Editable)
+                ));
+        };
+    }
+
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
 
         if (GUILayout.Button("Switch Prefabs"))
         {
-            PrefabSwitcher ps = (PrefabSwitcher)target;
             List<GameObject> oldObjs = ps.oldObjects;
             List<GameObject> newObjs = oldObjs.ConvertAll(go => ps.switchPrefab(go));
             Undo.RecordObjects(newObjs.ToArray(), "Create new objects with prefab: " + ps.newPrefab.name);
             Undo.RecordObjects(oldObjs.ToArray(), "Delete old objects");
             oldObjs.ForEach(go => DestroyImmediate(go));
+            ps.oldObjects.Clear();
+            ps.oldObjects.AddRange(newObjs);
         }
     }
 }
--- Assets/Scripts/Tools/PrefabSwitcher.cs
+++ Assets/Scripts/Tools/PrefabSwitcher.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PrefabSwitcher : MonoBehaviour
 {
@@ -15,10 +17,29 @@ public class PrefabSwitcher : MonoBehaviour
             newPrefab = this.newPrefab;
         }
         GameObject newObject = Instantiate(newPrefab);
+        newObject.transform.parent = oldObject.transform.parent;
         newObject.transform.position = oldObject.transform.position;
         newObject.transform.up = oldObject.transform.up;
         newObject.transform.localScale = oldObject.transform.localScale;
         newObject.name = oldObject.name;
         return newObject;
     }
+
+    public void setOldObjs(List<GameObject> oldObjs)
+    {
+        Debug.Log("oldObjs count: " + oldObjs.Count);
+        //If any of the selected objects has a prefab switcher
+        if (oldObjs.Any(go => go.GetComponent<PrefabSwitcher>() != null))
+        {
+            //Don't do anything
+            return;
+        }
+        this.oldObjects.Clear();
+        oldObjs
+            .FindAll(go => go is GameObject)
+            .ForEach(
+                go => oldObjects.Add((GameObject)go)
+                );
+    }
 }
+
