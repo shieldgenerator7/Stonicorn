399b194813468898f2aba86efc562d14a1d8c9c1
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-11-23 20:41:32 -0500

    Add Prefab Switcher

---
+++ Assets/Editor/PrefabSwitcherEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PrefabSwitcher))]
+public class PrefabSwitcherEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Switch Prefabs"))
+        {
+            PrefabSwitcher ps = (PrefabSwitcher)target;
+            List<GameObject> oldObjs = ps.oldObjects;
+            List<GameObject> newObjs = oldObjs.ConvertAll(go => ps.switchPrefab(go));
+            Undo.RecordObjects(newObjs.ToArray(), "Create new objects with prefab: " + ps.newPrefab.name);
+            Undo.RecordObjects(oldObjs.ToArray(), "Delete old objects");
+            oldObjs.ForEach(go => DestroyImmediate(go));
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/PrefabSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PrefabSwitcher : MonoBehaviour
+{
+    public GameObject newPrefab;
+    public List<GameObject> oldObjects;
+
+    public GameObject switchPrefab(GameObject oldObject, GameObject newPrefab = null)
+    {
+        if (newPrefab == null)
+        {
+            newPrefab = this.newPrefab;
+        }
+        GameObject newObject = Instantiate(newPrefab);
+        newObject.transform.position = oldObject.transform.position;
+        newObject.transform.up = oldObject.transform.up;
+        newObject.transform.localScale = oldObject.transform.localScale;
+        newObject.name = oldObject.name;
+        return newObject;
+    }
+}
