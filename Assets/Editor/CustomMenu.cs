e37f3316924d0db07a918dd0be37511420599fec
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-05 06:53:52 -0500

    Move FindMissingScripts into Custom Menu

--- Assets/Editor/CustomMenu.cs
+++ Assets/Editor/CustomMenu.cs
@@ -19,6 +19,54 @@ public class CustomMenu
         }
     }
 
+    //Find Missing Scripts
+    //2018-04-13: copied from http://wiki.unity3d.com/index.php?title=FindMissingScripts
+    static int go_count = 0, components_count = 0, missing_count = 0;
+    [MenuItem("SG7/Editor/Refactor/Find Missing Scripts")]
+    private static void FindMissingScripts()
+    {
+        go_count = 0;
+        components_count = 0;
+        missing_count = 0;
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene s = SceneManager.GetSceneAt(i);
+            if (s.isLoaded)
+            {
+                foreach (GameObject go in s.GetRootGameObjects())
+                {
+                    FindInGO(go);
+                }
+            }
+        }
+        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", go_count, components_count, missing_count));
+    }
+    private static void FindInGO(GameObject g)
+    {
+        go_count++;
+        Component[] components = g.GetComponents<Component>();
+        for (int i = 0; i < components.Length; i++)
+        {
+            components_count++;
+            if (components[i] == null)
+            {
+                missing_count++;
+                string s = g.name;
+                Transform t = g.transform;
+                while (t.parent != null)
+                {
+                    s = t.parent.name + "/" + s;
+                    t = t.parent;
+                }
+                Debug.Log(s + " has an empty script attached in position: " + i, g);
+            }
+        }
+        // Now recurse through each child GO (if there are any):
+        foreach (Transform childT in g.transform)
+        {
+            FindInGO(childT.gameObject);
+        }
+    }
 
     [MenuItem("SG7/Editor/Refactor/Change HideableArea to NonTeleportableArea")]
     public static void changeTag()
@@ -78,7 +126,7 @@ public class CustomMenu
         }
     }
 
-    [MenuItem("SG7/Editor/Toggle Merky Spawn Point %#`")]
+    [MenuItem("SG7/Editor/Spawn Point/Toggle Merky Spawn Point %#`")]
     public static void callMerky()
     {
         if (Application.isEditor && !Application.isPlaying)
@@ -116,7 +164,7 @@ public class CustomMenu
         }
     }
 
-    [MenuItem("SG7/Editor/Deactivate Merky Spawn Point %&`")]
+    [MenuItem("SG7/Editor/Spawn Point/Deactivate Merky Spawn Point %&`")]
     public static void uncallMerky()
     {
         if (Application.isEditor && !Application.isPlaying)
@@ -132,7 +180,7 @@ public class CustomMenu
         }
     }
 
-    [MenuItem("SG7/Editor/Toggle Ruler %`")]
+    [MenuItem("SG7/Editor/Ruler/Toggle Ruler %`")]
     /// <summary>
     /// Turns the ruler tools on and off
     /// </summary>
@@ -163,7 +211,7 @@ public class CustomMenu
         }
     }
 
-    [MenuItem("SG7/Editor/Call Ruler to its Range Preview #`")]
+    [MenuItem("SG7/Editor/Ruler/Call Ruler to its Range Preview #`")]
     /// <summary>
     /// Repositions the ruler to its range preview's position
     /// </summary>
@@ -174,7 +222,7 @@ public class CustomMenu
             rrp.callParentRuler();
         }
     }
-    [MenuItem("SG7/Editor/Toggle Ruler Range Preview &`")]
+    [MenuItem("SG7/Editor/Ruler/Toggle Ruler Range Preview &`")]
     /// <summary>
     /// Turns the ruler tools on and off
     /// </summary>
