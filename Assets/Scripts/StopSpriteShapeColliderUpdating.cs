2e3ed32adf0b3b85777f8c05385e29ea6ed0f7a2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-04 19:22:15 -0500

    Stop SpriteShapes from updating colliders every frame

---
+++ Assets/Scripts/StopSpriteShapeColliderUpdating.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.U2D;
+
+public class StopSpriteShapeColliderUpdating : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find all SpriteShapes now
+        FindObjectsOfType<SpriteShapeController>().ToList()
+            .ForEach(ssc => ssc.autoUpdateCollider = false);
+        //Find all SpriteShapes when a scene is loaded
+        SceneManager.sceneLoaded += (scene, loadMode) =>
+        FindObjectsOfType<SpriteShapeController>().ToList()
+            .ForEach(ssc => ssc.autoUpdateCollider = false);
+        Destroy(this);
+    }
+}
