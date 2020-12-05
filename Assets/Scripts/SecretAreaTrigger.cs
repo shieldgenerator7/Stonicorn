b805980e9d68dabb72ea25b1618d8f68f3b1ad84
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-04 21:55:04 -0500

    Simplify SecretAreaTrigger

--- Assets/Scripts/SecretAreaTrigger.cs
+++ Assets/Scripts/SecretAreaTrigger.cs
@@ -3,28 +3,12 @@ using System.Collections;
 
 public class SecretAreaTrigger : MonoBehaviour
 {
-
-    public GameObject secretHider;
-
-    // Use this for initialization
-    void Start()
-    {
-        if (secretHider == null)//if the secret hider isn't set, then it's probably its parent
-        {
-            secretHider = transform.parent.gameObject;
-        }
-    }
-
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (!coll.isTrigger && coll.gameObject.isPlayer())
+        if (coll.isPlayerSolid())
         {
-            if (secretHider != null && !ReferenceEquals(secretHider, null))
-            {
-                HiddenArea ha;
-                ha = secretHider.GetComponent<HiddenArea>();
-                ha.Discovered = true;
-            }
+            GetComponentInParent<HiddenArea>()
+                .Discovered = true;
         }
     }
 }
