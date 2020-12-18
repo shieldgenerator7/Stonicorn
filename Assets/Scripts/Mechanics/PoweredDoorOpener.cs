a9d0d84443e0f43ac2c4dc42ed86d359fc745d29
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-18 04:51:14 -0500

    Add primitive version of new door opener prefab

---
+++ Assets/Scripts/Mechanics/PoweredDoorOpener.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredDoorOpener : MonoBehaviour, IPowerable
+{
+    public float maxEnergyPerSecond = 3;
+    public float moveForce = 10;//magnitude
+    private Vector2 moveVector;//direction
+
+    public Collider2D moveColl;
+
+    public float ThroughPut => maxEnergyPerSecond;
+    public GameObject GameObject => gameObject;
+
+    private OnPowerFlowed onPowerGiven;
+    public OnPowerFlowed OnPowerFlowed
+    {
+        get => onPowerGiven;
+        set => onPowerGiven = value;
+    }
+
+    private RaycastHit2D[] rch2dStartup = new RaycastHit2D[Utility.MAX_HIT_COUNT];
+
+    // Use this for initialization
+    void Start()
+    {
+        moveVector = transform.up;
+        //Error checking
+        if (!moveColl.isTrigger)
+        {
+            Debug.LogError("PoweredDoorOpener.moveColl requires a collider that is a trigger!", gameObject);
+        }
+    }
+
+    public float acceptPower(float power)
+    {
+        float maxEnergy = maxEnergyPerSecond * Time.fixedDeltaTime;
+        float energyToUse = Mathf.Min(power, maxEnergy);
+        if (energyToUse > 0)
+        {
+            Vector3 forceVector = (energyToUse / maxEnergy) * moveForce * moveVector;
+            //Push objects in zone
+            int count = Utility.Cast(moveColl, Vector2.zero, rch2dStartup);
+            for (int i = 0; i < count; i++)
+            {
+                Rigidbody2D rb2d = rch2dStartup[i].collider.gameObject.GetComponent<Rigidbody2D>();
+                if (rb2d)
+                {
+                    rb2d.velocity = forceVector;
+                }
+            }
+        }
+        onPowerGiven?.Invoke(energyToUse, maxEnergy);
+        return power - energyToUse;
+    }
+}
