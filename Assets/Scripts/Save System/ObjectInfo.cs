b32340481bfafdaef58bc7200fe123c12c7a69ba
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-23 06:34:44 -0500

    Add ObjectInfo

---
+++ Assets/Scripts/Save System/ObjectInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains information about a GameObject
+/// to make it work better with the save system
+/// </summary>
+public class ObjectInfo : MonoBehaviour
+{
+    [SerializeField]
+    private string prefabName;
+    public string PrefabName => prefabName;
+}
