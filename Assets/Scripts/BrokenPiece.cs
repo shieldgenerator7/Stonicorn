9271cf88960233736f89d13c2f6b7f15638e40cb
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-12-24 04:26:37 -0500

    Extract BrokenPiece from BreakableWall

---
+++ Assets/Scripts/BrokenPiece.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Breaks apart a spawned prefab into its child pieces
+/// </summary>
+public class BrokenPiece : MonoBehaviour
+{
+    public void init(GameObject original)
+    {
+        string spawnTag = "---" + System.DateTime.Now.Ticks;
+        Scene scene = original.scene;
+
+        //Initialize this object
+        name += spawnTag;
+        transform.position = original.transform.position;
+        transform.rotation = original.transform.rotation;
+        SceneLoader.moveToScene(gameObject, scene);
+
+        //Initialize child objects
+        Vector3 origScale = original.transform.localScale;
+        SpriteRenderer origSR = original.GetComponent<SpriteRenderer>();
+        foreach (Transform t in transform)
+        {
+            GameObject go = t.gameObject;
+            //Spawn Tag
+            go.name += spawnTag;
+            //Scale and Position
+            t.localScale = new Vector3(
+                t.localScale.x * origScale.x,
+                t.localScale.y * origScale.y,
+                t.localScale.z * origScale.z
+            );
+            t.localPosition = new Vector2(
+                t.localPosition.x * t.localScale.x,
+                t.localPosition.y * t.localScale.y
+                );
+            //SpriteRenderer
+            SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+            sr.color = origSR.color;
+            sr.sortingLayerID = origSR.sortingLayerID;
+            sr.sortingOrder = origSR.sortingOrder;
+            //Copyables
+            foreach (ICopyable copy in original.GetComponents<ICopyable>())
+            {
+                ICopyable mb = (ICopyable)go.GetComponent(copy.CopyableType);
+                if (mb != null)
+                {
+                    mb.copyFrom(original);
+                }
+            }
+            //Unparent it
+            t.SetParent(null);
+            //Put it in the scene
+            SceneLoader.moveToScene(go, scene);
+            //Register it
+            Managers.Object.addObject(go);
+        }
+    }
+}
---
+++ Assets/Scripts/Interfaces/ICopyable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICopyable
+{
+    System.Type CopyableType { get; }
+    void copyFrom(GameObject original);
+}
