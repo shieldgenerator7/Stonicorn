10002ce941ee6f8ac08bea91520f3a21b6323543
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-04 01:00:49 -0500

    Add TriggerActivatorTrigger

---
+++ Assets/Scripts/Tutorial/TriggerActivatorTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerActivatorTrigger : ActivatorTrigger
+{
+    public bool forPlayerOnly = true;
+
+    private bool triggered = false;
+    public override bool Triggered => triggered;
+
+    private void OnTriggerEnter2D(Collider2D coll2d)
+    {
+        if (!forPlayerOnly || coll2d.isPlayerSolid())
+        {
+            triggered = true;
+            triggeredChanged();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll2d)
+    {
+        if (!forPlayerOnly || coll2d.isPlayerSolid())
+        {
+            triggered = false;
+            triggeredChanged();
+        }
+    }
+}
