6c513cdd2b15a4b68a211226e08a600526e6bbd5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-06 07:00:48 -0500

    Add SceneLoaderEditor*
    *to autoset Scene buildindex so I don't have to mess with it

---
+++ Assets/Editor/SceneLoaderEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CustomEditor(typeof(SceneLoader))]
+[CanEditMultipleObjects]
+public class SceneLoaderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUI.enabled = !EditorApplication.isPlaying;
+        if (GUILayout.Button("Autoset Scene Id (Edit Mode)"))
+        {
+            targets.ToList().ForEach(t =>
+            {
+                SceneLoader sl = (SceneLoader)t;
+                Scene scene = EditorSceneManager.GetSceneByName(sl.sceneName);
+                sl.sceneId = scene.buildIndex;
+                EditorUtility.SetDirty(sl);
+                EditorSceneManager.MarkSceneDirty(scene);
+            });
+        }
+    }
+}
