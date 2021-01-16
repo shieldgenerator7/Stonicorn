7f5e820f5747b20416075f10ce754cf7fdc2acb7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-16 18:15:40 -0500

    Remove ICopyable

--- Assets/Scripts/Interfaces/ICopyable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICopyable
-{
-    System.Type CopyableType { get; }
-    void copyFrom(GameObject original);
-}
