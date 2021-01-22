2f80864f82a01f69697167fbc31bbd1128dc94fe
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-21 22:01:55 -0500

    Fix #513 play button default zoom level on 2nd time

---
+++ Assets/Scripts/Menu/ButtonSpecific/MenuActionButtonPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActionButtonPlay : MenuActionButton
+{
+    public override void activate()
+    {
+        if (Managers.Stats.get(Stat.MENU_BUTTON_PLAY) == 0)
+        {
+            //First time:
+            //Zoom out to radius
+            Managers.Camera.ZoomScalePoint = CameraController.CameraScalePoints.RANGE;
+        }
+        else
+        {
+            //Following time:
+            //Zoom out to default
+            Managers.Camera.ZoomScalePoint = CameraController.CameraScalePoints.DEFAULT;
+        }
+        Managers.Stats.addOne(Stat.MENU_BUTTON_PLAY);
+    }
+}
