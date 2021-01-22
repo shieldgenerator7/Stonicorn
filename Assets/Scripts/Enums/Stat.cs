8cd67f64cebaa1ea1118b27ab275a05a99a92a30
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-01-21 21:13:38 -0500

    Add regions to Stat enums

--- Assets/Scripts/Enums/Stat.cs
+++ Assets/Scripts/Enums/Stat.cs
@@ -2,11 +2,14 @@
 
 public enum Stat
 {
+    #region Touch Input
     TAP,//how many times the player has tapped
     HOLD,//how many times the player has done the hold gesture
     DRAG,//how many times the player has done the drag gesture
     PINCH,//how many times the player has done the pinch gesture (including mouse wheel)
+    #endregion
 
+    #region Abilities
     TELEPORT,//how many times the player has teleported
     FORCE_LAUNCH_LAUNCH,//how many times the player has launched with the force launch ability
     SWAP,//how many times the player has swapped during a teleport
@@ -18,8 +21,11 @@ public enum Stat
     AIR_SLICE,//how many times the player has teleported in the air with the air slice ability
     AIR_SLICE_OBJECT,//how many objects the player has sliced
     LONG_TELEPORT,//how many times the player has teleported with an extended range
+    #endregion
 
+    #region Rewind
     DAMAGED,//how many times the player has been damaged
     REWIND,//how many times the rewind ability has been activated
     REWIND_PLAYER//how many times the player used the rewind ability
+    #endregion
 }
