493bb1460445ff019546b3f7836f46889c6aea51
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-03-09 05:11:33 -0500

    Rename to DistanceYTimer

--- Assets/Scripts/DistanceYTimer.cs
+++ Assets/Scripts/DistanceYTimer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DistanceTimer : Timer
+public class DistanceYTimer : Timer
 {
     [Header("Distance Timer Settings")]
     public Transform timerObject;
