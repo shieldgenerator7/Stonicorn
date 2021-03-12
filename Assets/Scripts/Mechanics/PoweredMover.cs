2d8eb69a6819c533ab7771182046b5926838277f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-03-12 13:14:35 -0500

    Add powered mover prefab

---
+++ Assets/Scripts/Mechanics/PoweredMover.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoweredMover : SavableMonoBehaviour, IPowerable
+{
+    public float maxEnergyPerSecond = 3;
+    public float moveForce = 10;//magnitude
+    public Vector2 moveVector;//direction, relative to self
+
+    private Rigidbody2D rb2d;
+
+    public float ThroughPut => maxEnergyPerSecond;
+    public GameObject GameObject => gameObject;
+
+    private OnPowerFlowed onPowerGiven;
+    public OnPowerFlowed OnPowerFlowed
+    {
+        get => onPowerGiven;
+        set => onPowerGiven = value;
+    }
+    public override SavableObject CurrentState
+    {
+        get => new SavableObject(this);
+        set { }
+    }
+    public override void init()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public float acceptPower(float power)
+    {
+        float maxEnergy = maxEnergyPerSecond * Time.fixedDeltaTime;
+        float energyToUse = Mathf.Min(power, maxEnergy);
+        if (energyToUse > 0)
+        {
+            //Move self
+            Vector3 forceVector = (energyToUse / maxEnergy) * moveForce * transform.TransformDirection(moveVector);
+            rb2d.velocity = forceVector;
+        }
+        onPowerGiven?.Invoke(energyToUse, maxEnergy);
+        return power - energyToUse;
+    }
+}
