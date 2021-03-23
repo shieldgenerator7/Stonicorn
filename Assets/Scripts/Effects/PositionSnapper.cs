6e80fc1bbfd49f8b357576e64313121ca87f7bef
shieldgenerator7 <shieldgenerator7@yahoo.com>
2021-03-22 21:46:26 -0400

    Add Electric Beam tutorial

--- Assets/Scripts/Effects/PositionSnapper.cs
+++ Assets/Scripts/Effects/PositionSnapper.cs
@@ -6,6 +6,7 @@ public class PositionSnapper : MonoBehaviour
 {
 
     public bool snapToPlayerGhosts = true;
+    public bool snapToPlayer = false;
     public float range = -1;
     public Vector2 offset = Vector2.zero;
 
@@ -24,6 +25,22 @@ public class PositionSnapper : MonoBehaviour
             Vector2 position = Managers.PlayerRewind
                 .getClosestPlayerGhost(startPosition)
                 .transform.position;
+            if (range < 0 || position.inRange(transform.position, range))
+            {
+                transform.position = position + offset;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (snapToPlayer)
+        {
+            Vector2 position = Managers.Player.transform.position;
             if (range < 0 || position.inRange(transform.position, range))
             {
                 transform.position = position + offset;
