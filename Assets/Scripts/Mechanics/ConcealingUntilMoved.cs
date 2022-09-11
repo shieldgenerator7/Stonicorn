ccc00c870b7b9530a58d9645b932002c8f1e0cb2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-09-11 03:26:12 -0400

    Add small boulders that block hidden areas

---
+++ Assets/Scripts/Mechanics/ConcealingUntilMoved.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcealingUntilMoved : MonoBehaviour
+{
+    public List<HiddenArea> haListToUncover;
+
+    private Vector2 startPos;
+    private StaticUntilTouched staticUntilTouched;
+
+    private void Start()
+    {
+        //Record startPos
+        startPos = transform.position;
+        //Register with staticUntilTouched (if available)
+        staticUntilTouched = GetComponent<StaticUntilTouched>();
+        if (staticUntilTouched)
+        {
+            staticUntilTouched.onRootedChanged += (rooted) =>
+            {
+                if (!rooted)
+                {
+                    revealHiddenAreas();
+                }
+            };
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        checkRevealHiddenAreas();
+    }
+
+    private void checkRevealHiddenAreas()
+    {
+        Vector2 currentPosition = transform.position;
+        if (currentPosition != startPos)
+        {
+            revealHiddenAreas();
+        }
+    }
+
+    private void revealHiddenAreas()
+    {
+        haListToUncover.ForEach(ha => ha.Discovered = true);
+        Destroy(this);
+    }
+}
