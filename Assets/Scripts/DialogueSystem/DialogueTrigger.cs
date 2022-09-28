f47adf931229679e4f37251e44f1a0491c96c3e6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-09-27 23:10:30 -0400

    Copy more dialogue code from Grimm's Wish

---
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Triggers a dialogue cutscene
+/// </summary>
+public class DialogueTrigger : EventTrigger
+{
+    public string title;
+    public List<string> characters;
+
+    public override bool Interactable
+    {
+        get
+        {
+            //Find dialogue path by its title
+            if (title != "" && title != null)
+            {
+                return dialogueManager.hasDialogue(title);
+            }
+            //Find dialogue path by characters
+            else
+            {
+                return dialogueManager.hasDialogue(characters);
+            }
+        }
+    }
+
+    protected override void triggerEvent()
+    {
+        //Find dialogue path by its title
+        if (title != "" && title != null)
+        {
+            dialogueManager.playDialogue(title);
+        }
+        //Find dialogue path by characters
+        else
+        {
+            dialogueManager.playDialogue(characters);
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueSystem/ItemCollectTrigger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectTrigger : EventTrigger
+{
+    [Tooltip("The title of the dialogue to play")]
+    public string title;
+    [Tooltip("The variable to increment by 1 if there is no title")]
+    public string counterName;
+
+    protected override void Start()
+    {
+        //If this item has already been collected,
+        if (FindObjectOfType<DialogueManager>().progressManager.hasActivated(this))
+        {
+            //Destroy it
+            destroy();
+        }
+
+        //Parent start up process
+        base.Start();
+
+        //Item must have a title or a counterName
+        if ((title == null || title == "")
+            && (counterName == null || counterName == ""))
+        {
+            Debug.LogError(
+                "ItemCollectTrigger must have either a title or a counterName."
+                , this);
+        }
+    }
+
+    protected override void triggerEvent()
+    {
+        //Find dialogue path by its title
+        if (title != "" && title != null)
+        {
+            dialogueManager.playDialogue(title);
+        }
+        else
+        {
+            dialogueManager.progressManager.add(counterName);
+        }
+        destroy();
+    }
+
+    private void destroy()
+    {
+        Destroy(this);
+        Destroy(transform.parent.gameObject);
+    }
+}
