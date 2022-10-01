d8a50db266807834b0839f54169636b486aa1e60
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-01 02:59:33 -0400

    Get dialogue box sizing correctly with TMP_Text

--- Assets/Scripts/DialogueSystem/DialogueBoxUpdater.cs
+++ Assets/Scripts/DialogueSystem/DialogueBoxUpdater.cs
@@ -47,11 +47,13 @@ public class DialogueBoxUpdater : MonoBehaviour
         Start();
 #endif
         this.rawText = text;
-
-        Vector2 messageDimensions = getMessageDimensions(canvas, txtDialogue, text);
-        int maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
-        text = processMessage(canvas, txtDialogue, text, maxTextLength);
+        
+        //txtDialogue.text = text;
+        //Vector2 messageDimensions = getMessageDimensions(canvas, txtDialogue, text);
+        //int maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
+        //text = processMessage(canvas, txtDialogue, text, maxTextLength);
         txtDialogue.text = text;
+        txtDialogue.ForceMeshUpdate();
         updateSize();
         updatePosition();
     }
@@ -98,23 +100,24 @@ public class DialogueBoxUpdater : MonoBehaviour
 
     private void updateSize()
     {
-        string text = this.rawText;
-        //size textbox
-        float textHeight = getTextHeight(canvas, txtDialogue);
-        float buffer = textHeight / 2;
-        int maxTextLength = getMaxTextLength(canvas, txtDialogue);
-        Vector2 messageDimensions = getMessageDimensions(canvas, txtDialogue, text);
-        maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
-        if (messageDimensions.y > textHeight)
-        {
-            float textWidth = getTextWidth(canvas, txtDialogue, text);
-            int lineCount = Mathf.CeilToInt(textWidth / getMaxWidth(canvas, txtDialogue));
-            maxTextLength = (maxTextLength + (text.Length / lineCount)) / 2;
-            messageDimensions = getMessageDimensions(canvas, txtDialogue, text, maxTextLength);
-            maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
-        }
-        Vector2 textBoxSize = messageDimensions + (Vector2.one * buffer * 2);
+        //string text = this.rawText;
+        ////size textbox
+        //float textHeight = getTextHeight(canvas, txtDialogue);
+        //float buffer = textHeight / 2;
+        //int maxTextLength = getMaxTextLength(canvas, txtDialogue);
+        //Vector2 messageDimensions = getMessageDimensions(canvas, txtDialogue, text);
+        //maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
+        //if (messageDimensions.y > textHeight)
+        //{
+        //    float textWidth = getTextWidth(canvas, txtDialogue, text);
+        //    int lineCount = Mathf.CeilToInt(textWidth / getMaxWidth(canvas, txtDialogue));
+        //    maxTextLength = (maxTextLength + (text.Length / lineCount)) / 2;
+        //    messageDimensions = getMessageDimensions(canvas, txtDialogue, text, maxTextLength);
+        //    maxTextLength = getTextLength(canvas, txtDialogue, messageDimensions.x);
+        //}
+        Vector2 textBoxSize = getTextSize(canvas, txtDialogue);// messageDimensions + (Vector2.one * buffer * 2);
         quoteSR.size = textBoxSize;
+        quoteBoxTail.transform.position = quoteSR.transform.position - (quoteBox.transform.up * quoteSR.size.y / 2);
     }
 
     public void resizeToCamera()
@@ -137,56 +140,6 @@ public class DialogueBoxUpdater : MonoBehaviour
         dialogueRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, cam.pixelHeight * 3 / 4);
     }
 
-    static float getTextWidth(Canvas canvas, TMP_Text text, string stringToMeasure)
-    {
-        float width = text.GetRenderedValues(true).x * canvas.transform.localScale.x;
-        return width;
-        /*
-        string prevString = text.text;
-        //text.text = stringToMeasure;
-        //2019-02-28: copied from an answer by pineda100: https://answers.unity.com/questions/921726/how-to-get-the-size-of-a-unityengineuitext-for-whi.html
-        TextGenerator textGen = new TextGenerator();
-        TextGenerationSettings generationSettings = text.GetGenerationSettings(text.rectTransform.rect.size);
-        float width = textGen.GetPreferredWidth(stringToMeasure, generationSettings);
-        float height = textGen.GetPreferredHeight(stringToMeasure, generationSettings);
-        Vector2 size = new Vector2(width, height);
-        size = Camera.main.ScreenToWorldPoint(size) - Camera.main.ScreenToWorldPoint(Vector2.zero);
-        size.x = Mathf.Abs(size.x);
-        size.y = Mathf.Abs(size.y);
-        text.text = prevString;
-        return size.x;*/
-        //return text.fontSize * 0.5f * length * canvas.transform.localScale.x;
-    }
-    /// <summary>
-    /// Adds up all the character offsets for the given string using the supplied text's font settings.
-    /// Should ideally return the total width, in pixels, that the string takes up when rendered.
-    /// </summary>
-    /// <param name="text">Text object used to get font settings.</param>
-    /// <param name="textString">String from which the actual characters to measure are extracted.</param>
-    /// <returns></returns>
-    static float getSumOfCharacterOffsets(Text text, string textString)
-    {
-        int totalMaxWidthLength = 0;
-
-        for (int i = 0; i < textString.Length; i++)
-        {
-            CharacterInfo chInfo;
-            if (text.font.GetCharacterInfo(textString[i], out chInfo, text.fontSize))
-            {
-                totalMaxWidthLength += chInfo.advance;
-            }
-            else
-            {
-                // Tried to access invalid character!  Stick a warning here if you care about that.
-            }
-        }
-
-        return totalMaxWidthLength;
-    }
-    static float getTextHeight(Canvas canvas, TMP_Text text, int lines = 1)
-    {
-        return text.fontSize * lines * canvas.transform.localScale.y;
-    }
     static float getMaxWidth(Canvas canvas, TMP_Text text)
     {
         return Mathf.Min(Screen.width / 2, text.rectTransform.rect.width * canvas.transform.localScale.x);
@@ -201,60 +154,41 @@ public class DialogueBoxUpdater : MonoBehaviour
         return getTextLength(canvas, text, getMaxWidth(canvas, text));
     }
 
-
-    static Vector2 getMessageDimensions(Canvas canvas, TMP_Text text, string message, int maxTextLength = 0)
+    static Vector2 getTextSize(Canvas canvas, TMP_Text text)
     {
-        List<string> strings = splitIntoSegments(canvas, text, message, maxTextLength);
-        string foundMaxString = "";
-        foreach (string s in strings)
-        {
-            if (s.Length > foundMaxString.Length)
-            {
-                foundMaxString = s;
-            }
-        }
-        float textWidth = getTextWidth(canvas, text, foundMaxString);
-        float textHeight = getTextHeight(canvas, text, strings.Count);
-        return new Vector2(textWidth, textHeight);
+        //assumes canvas scale x and y are the same
+        Vector2 size = text.GetRenderedValues(true) * canvas.transform.localScale.x;
+        Debug.Log($"getTextSize: {size}");
+        return size;
     }
 
-    static string processMessage(Canvas canvas, TMP_Text text, string message, int maxTextLength)
-    {
-        List<string> strings = splitIntoSegments(canvas, text, message, maxTextLength);
-        string buildString = "";
-        foreach (string str in strings)
-        {
-            string str2 = addSpaces(str, maxTextLength - str.Length);
-            buildString += $"{str2}\n";
-        }
-        buildString.Trim();
-        return buildString;
-    }
-    static string addSpaces(string original, int spaceCount)
-    {
-        string buildString = original;
-        for (int i = 0; i < spaceCount; i++)
-        {
-            buildString += " ";
-        }
-        return buildString;
-    }
+    //static string processMessage(Canvas canvas, TMP_Text text, string message, int maxTextLength)
+    //{
+    //    List<string> strings = splitIntoSegments(canvas, text, message, maxTextLength);
+    //    string buildString = "";
+    //    foreach (string str in strings)
+    //    {
+    //        buildString += $"{str}\n";
+    //    }
+    //    buildString.Trim();
+    //    return buildString;
+    //}
 
-    static List<string> splitIntoSegments(Canvas canvas, TMP_Text text, string message, int maxTextLength = 0)
-    {
-        float textWidth = getTextWidth(canvas, text, message);
-        float maxWidth = getMaxWidth(canvas, text);
-        int segmentLength = message.Length;
-        if (textWidth > maxWidth)
-        {
-            if (maxTextLength == 0)
-            {
-                maxTextLength = getMaxTextLength(canvas, text);
-            }
-            segmentLength = maxTextLength;
-        }
-        return splitIntoSegments(message, segmentLength);
-    }
+    //static List<string> splitIntoSegments(Canvas canvas, TMP_Text text, int maxTextLength = 0)
+    //{
+    //    float textWidth = getTextSize(canvas, text).x;
+    //    float maxWidth = getMaxWidth(canvas, text);
+    //    int segmentLength = message.Length;
+    //    if (textWidth > maxWidth)
+    //    {
+    //        if (maxTextLength == 0)
+    //        {
+    //            maxTextLength = getMaxTextLength(canvas, text);
+    //        }
+    //        segmentLength = maxTextLength;
+    //    }
+    //    return splitIntoSegments(message, segmentLength);
+    //}
     static List<string> splitIntoSegments(string message, int maxLength)
     {
         string[] split = message.Split(' ');
