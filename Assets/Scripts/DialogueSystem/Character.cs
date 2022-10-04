47a1ff3897352102efb86ee6f5845f3c0d690b0d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-04 00:05:14 -0400

    Add Character component to Merky prefab

--- Assets/Scripts/DialogueSystem/Character.cs
+++ Assets/Scripts/DialogueSystem/Character.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Character : MonoBehaviour
 {
     public string characterName;
