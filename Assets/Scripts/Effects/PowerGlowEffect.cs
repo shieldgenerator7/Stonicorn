03f7f514d98a619b10944b33d456eea83aa4264b
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-07-21 23:57:20 -0400

    #600 make new spriteshape wires glow

--- Assets/Scripts/Effects/PowerGlowEffect.cs
+++ Assets/Scripts/Effects/PowerGlowEffect.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.U2D;
 
 /// <summary>
 /// Creates a glow effect when the IPowerConduit in its parent object has power
 /// </summary>
 public class PowerGlowEffect : MonoBehaviour
 {
+    public Color effectColor = Color.white;
+
     private IPowerConduit conduit;
     private SpriteRenderer lightEffectRenderer;
+    private SpriteShapeRenderer spriteShapeRenderer;
     private TMP_Text text;
+
+    private Color startColor;
+
     // Start is called before the first frame update
     void Start()
     {
         lightEffectRenderer = GetComponent<SpriteRenderer>();
+        spriteShapeRenderer = GetComponent<SpriteShapeRenderer>();
         text = GetComponent<TMP_Text>();
-        conduit = GetComponent<IPowerConduit>();
-        if (conduit == null)
-        {
-            conduit = GetComponentInParent<IPowerConduit>();
-        }
+        conduit = GetComponent<IPowerConduit>() ?? GetComponentInParent<IPowerConduit>();
         conduit.OnPowerFlowed += onPowerFlowed;
+
+        startColor = spriteShapeRenderer?.color ?? Color.white;
     }
 
     private void OnDestroy()
@@ -45,6 +51,16 @@ public class PowerGlowEffect : MonoBehaviour
         if (lightEffectRenderer)
         {
             lightEffectRenderer.color = lightEffectRenderer.color.adjustAlpha(newAlpha);
+        }
+        if (spriteShapeRenderer)
+        {
+            Color color = new Color(0, 0, 0, 0);
+            float percent = Mathf.Clamp(power / maxPower, 0, 1);
+            color.r = Mathf.Lerp(startColor.r, effectColor.r, percent);
+            color.g = Mathf.Lerp(startColor.g, effectColor.g, percent);
+            color.b = Mathf.Lerp(startColor.b, effectColor.b, percent);
+            color.a = Mathf.Lerp(startColor.a, effectColor.a, percent);
+            spriteShapeRenderer.color = color;
         }
         if (text)
         {
