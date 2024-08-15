0dcf9dfc5bcab3d99ac57c89aea66868418b4aa2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-08-15 02:02:45 -0400

    #607 add BreakableVase

---
+++ Assets/Scripts/Mechanics/BreakableVase.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableVase : SavableMonoBehaviour, IBlastable
+{
+    public float destroyDelay = 1;
+    public float minForceThreshold = 2.5f;//the minimum amount of force required to crack it
+    public float maxForceThreshold = 50;//the maximum amount of force consumed per damage
+    [Range(1, 100)]
+    public float maxIntegrity = 3;
+    [SerializeField]
+    [Range(0, 100)]
+    private float integrity = 0;
+    public float Integrity
+    {
+        get => integrity;
+        set
+        {
+            integrity = Mathf.Clamp(value, 0, maxIntegrity);
+
+            if (integrity > 0)
+            {
+                if (crackStages.Count > 0)
+                {
+                    if (sr == null)
+                    {
+                        sr = GetComponent<SpriteRenderer>();
+                    }
+                    int index = (int)Mathf.Clamp(
+                        maxIntegrity - integrity,
+                        0,
+                        crackStages.Count - 1
+                        );
+                    sr.sprite = crackStages[index];
+                }
+            }
+            else
+            {
+                //Break into pieces
+                if (crackedPrefab)
+                {
+                    GameObject pieces = Utility.Instantiate(crackedPrefab);
+                    BrokenPiece brokenPiece = pieces.GetComponent<BrokenPiece>();
+                    brokenPiece.unpack(gameObject);
+                    //change color of broken pieces
+                    Color color = GetComponent<SpriteRenderer>().color;
+                    brokenPiece.Savables.ForEach(
+                        bp => bp.GetComponent<SpriteRenderer>().color = color
+                        );
+                }
+
+                //Reveal hidden areas
+                dicoverables
+                    .FindAll(ha => ha != null && !ReferenceEquals(ha, null))
+                    .ForEach(ha => ha.Discovered = true);
+
+                //Destroy object
+                Timer.startTimer(destroyDelay, () =>
+                {
+                    Managers.Object.destroyObject(gameObject);
+                });
+            }
+        }
+    }
+
+    [Header("Cracked Components")]
+    private GameObject crackedPrefab;
+    private List<Sprite> crackStages = new List<Sprite>();
+    public AudioClip soundDamageNone;
+    public AudioClip soundDamageOne;
+    private AudioClip soundDamageTwoOrMore;
+    public List<MemoryMonoBehaviour> dicoverables;
+
+    //Components
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+    public override void init()
+    {
+        //Components
+        sr = GetComponent<SpriteRenderer>();
+        //Initialize integrity
+        if (integrity == 0)
+        {
+            Integrity = maxIntegrity;
+        }
+        else
+        {
+            Integrity = Integrity;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        ContactPoint2D[] cp2ds = new ContactPoint2D[1];
+        coll.GetContacts(cp2ds);
+        Rigidbody2D rb2d = coll.gameObject.GetComponent<Rigidbody2D>();
+        if (rb2d != null)
+        {
+            Vector2 relativeVelocity = coll.relativeVelocity;
+            float force = relativeVelocity.magnitude * rb2d.mass;
+            float damage = checkForce(force, relativeVelocity);
+            //Show Collision Effect
+            float hitPercentage = damage * 100 / maxIntegrity;
+            Managers.Effect.collisionEffect(cp2ds[0].point, hitPercentage);
+            //Play Collision Sound
+            if (damage == 0)
+            {
+                Managers.Sound.playSound(soundDamageNone, cp2ds[0].point, 0.5f);
+            }
+            else if (damage == 1)
+            {
+                Managers.Sound.playSound(soundDamageOne, cp2ds[0].point);
+            }
+            else if (damage >= 2)
+            {
+                Managers.Sound.playSound(soundDamageTwoOrMore, cp2ds[0].point);
+            }
+        }
+    }
+
+    public float checkForce(float force, Vector2 direction)
+    {
+        if (force >= minForceThreshold)
+        {
+            float thresholdRange = maxForceThreshold - minForceThreshold;
+            float damage = maxIntegrity * ((force - minForceThreshold) / thresholdRange);
+            Integrity -= damage;
+            return damage;
+        }
+        return 0;
+    }
+
+    public float getDistanceFromExplosion(Vector2 explosionPos)
+    {
+        return explosionPos.distanceToObject(gameObject);
+    }
+
+    public override SavableObject CurrentState
+    {
+        get => new SavableObject(this, "integrity", integrity);
+        set
+        {
+            Integrity = value.Int("integrity");
+        }
+    }
+}
