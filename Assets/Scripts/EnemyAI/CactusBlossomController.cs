6aeedde66243b74bf334a0d2720284cf84116fd0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-09 21:58:39 -0400

    #608 first version of placing petals

--- Assets/Scripts/EnemyAI/CactusBlossomController.cs
+++ Assets/Scripts/EnemyAI/CactusBlossomController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CactusBlossomController : MonoBehaviour
@@ -7,6 +8,8 @@ public class CactusBlossomController : MonoBehaviour
     public float openLeft = 0;
     [Tooltip("How far to the right to open to. 0 is pointing to its left")]
     public float openRight = 180;
+    [Range(0f, 1f)]
+    public float _openPercent = 1f;
 
     public List<Transform> petals;
 
@@ -19,11 +22,19 @@ public class CactusBlossomController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        placePetals(_openPercent);
     }
 
     public void placePetals(float openPercent)
     {
-
+        float openDiff = openRight - openLeft;
+        float openHalf = openDiff / 2;
+        for(int i = 0; i < petals.Count; i++)
+        {
+            Transform petal = petals[i];
+            float percent = (float)i / (float)(petals.Count - 1);
+            float angle = openDiff * percent + openLeft;
+            petal.localEulerAngles = new Vector3(0,0,angle);
+        }
     }
 }
