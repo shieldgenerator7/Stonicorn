eb35c26b9f7f2081683d4aa32e8f9711c41f0a97
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-09-09 22:53:42 -0400

    #608 cactus blossoms close when you teleport near them

--- Assets/Scripts/EnemyAI/CactusBlossomController.cs
+++ Assets/Scripts/EnemyAI/CactusBlossomController.cs
@@ -13,18 +13,60 @@ public class CactusBlossomController : MonoBehaviour
     [Range(0f, 1f)]
     public float _openPercent = 1f;
 
+    [Tooltip("How long it takes to close while closing")]
+    public float closingDuration = 0.5f;
+    [Tooltip("How long it stays closed")]
+    public float closedDuration = 5;
+    [Tooltip("How long it takes to open while opening")]
+    public float openingDuration = 2;
+
     public List<Transform> petals;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        placePetals(_openPercent);
     }
 
     // Update is called once per frame
     void Update()
     {
-        placePetals(_openPercent);
+        //placePetals(_openPercent);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TeleportAbility ta = collision.GetComponent<TeleportAbility>();
+        if (ta)
+        {
+            ta.onTeleport -= reactToTeleport;
+            ta.onTeleport += reactToTeleport;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        TeleportAbility ta = collision.GetComponent<TeleportAbility>();
+        if (ta)
+        {
+            ta.onTeleport -= reactToTeleport;
+        }
+    }
+
+    public void reactToTeleport(Vector2 oldPos, Vector2 newPos)
+    {
+        Timer closingTimer = Timer.startTimer(closingDuration, () => {
+            Timer closedTimer = Timer.startTimer(closedDuration, () => {
+                Timer openingTimer = Timer.startTimer(openingDuration, () => { });
+                openingTimer.onTimeLeftChanged += (timeLeft, duration) =>
+                {
+                    placePetals(1 - (timeLeft / duration));
+                };
+            });
+        });
+        closingTimer.onTimeLeftChanged += (timeLeft, duration) =>
+        {
+            placePetals(timeLeft / duration);
+        };
     }
 
     public void placePetals(float openPercent)
