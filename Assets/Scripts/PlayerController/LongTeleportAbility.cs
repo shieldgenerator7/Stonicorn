ebb0e3956a3a4ae1e462f12245405c2c625fb99a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-05 19:52:48 -0500

    Fix bug: shield expires early if recently hit

--- Assets/Scripts/PlayerController/LongTeleportAbility.cs
+++ Assets/Scripts/PlayerController/LongTeleportAbility.cs
@@ -29,6 +29,7 @@ public class LongTeleportAbility : PlayerAbility
                 playerController.onHazardHitException -= onHitException;
                 if (shielded)
                 {
+                    ShouldUnshield = false;
                     playerController.onHazardHitException += onHitException;
                     Debug.Log("OnHit registered!");
                 }
