4c7159df1a0a19f2eea6fc15dc0c3faa1e66ef46
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-05 21:25:26 -0500

    Flashlight now lasts a duration, resets on teleport

--- Assets/Scripts/PlayerController/FlashlightAbility.cs
+++ Assets/Scripts/PlayerController/FlashlightAbility.cs
@@ -24,7 +24,9 @@ public class FlashlightAbility : PlayerAbility
     public SpriteMask flashlightBeamMask;
     public SpriteRenderer flashlightPlayerGlowSR;
     private bool flashlightOn = false;
+    private bool flashAuraOn = false;
     private List<SpriteRenderer> flashlightSRs;
+    private Vector2 originalFlashlightDirection;
     private Vector2 flashlightDirection;
     public Vector2 FlashlightDirection
     {
@@ -38,6 +40,8 @@ public class FlashlightAbility : PlayerAbility
             }
         }
     }
+
+    Timer timer;
 
     public override void init()
     {
@@ -55,12 +59,34 @@ public class FlashlightAbility : PlayerAbility
     #region Input Processing
 
     protected override bool isGrounded() => false;
-    protected override void processTeleport(Vector2 oldPos, Vector2 newPos) { }
+    protected override void processTeleport(Vector2 oldPos, Vector2 newPos) {
+        if (flashlightOn || flashAuraOn)
+        {
+            //FlashlightDirection = originalFlashlightDirection;
+            //updateFlashlightVisuals();
+            updateFlashAuraVisuals(1, afterglowStartSize);
+            startFade();
+        }
+    }
 
     public void processDrag(Vector2 oldPos, Vector2 newPos, GestureState state)
     {
         flashlightOn = state == GestureState.ONGOING;
         FlashlightDirection = (Vector2)playerController.transform.position - newPos;
+        switch (state)
+        {
+            case GestureState.START:
+                afterglowStartSize = 0;
+                return;
+            case GestureState.ONGOING:
+                flashAuraOn = true;
+                originalFlashlightDirection = flashlightDirection;
+                break;
+            case GestureState.FINISH:
+                afterglowStartSize = flashlightPlayerGlowSR.transform.localScale.x;//assume x and y are same and the sprite takes a single unit
+                startFade();
+                break;
+        }
         updateFlashlightVisuals();
     }
     #endregion
@@ -81,20 +107,14 @@ public class FlashlightAbility : PlayerAbility
             size.y = maxBeamDistance * percent;
             flashlightBeamMask.transform.localScale = size;
 
-            Vector2 sizeGlow = flashlightPlayerGlowSR.transform.localScale;
-            sizeGlow = Vector2.one * ((maxGlowSize - minGlowSize) * (1 - percent) + minGlowSize);
-            flashlightPlayerGlowSR.transform.localScale = sizeGlow;
-
             //adjust alpha
             float alpha = (1 - percent) * (maxAlpha - minAlpha) + minAlpha;
             flashlightSRs.ForEach(flsr =>
                 flsr.color = flsr.color.adjustAlpha(alpha)
             );
 
-            if (Utility.between(glowAlpha, 0, 1))
-            {
-                flashlightPlayerGlowSR.color = flashlightPlayerGlowSR.color.adjustAlpha(glowAlpha);
-            }
+            //aura
+            updateFlashAuraVisuals(1-percent);
 
             //enable sprites
             flashlightSRs.ForEach(flsr => flsr.enabled = true);
@@ -105,21 +125,46 @@ public class FlashlightAbility : PlayerAbility
             flashlightSRs.ForEach(flsr =>
                 flsr.enabled = false
             );
-            flashlightPlayerGlowSR.enabled = true;
-
-            playerController.Teleport.onTeleport += startFade;
+            updateFlashAuraVisuals(1, afterglowStartSize);
+        }
+    }
+    void updateFlashAuraVisuals(float percent, float maxSize = 0)
+    {
+        flashlightPlayerGlowSR.enabled = percent > 0;
+        Vector2 sizeGlow = flashlightPlayerGlowSR.transform.localScale;
+        if (maxSize > 0)
+        {
+            sizeGlow = Vector2.one * ((afterglowStartSize) * percent);
+        }
+        else
+        {
+            sizeGlow = Vector2.one * ((maxGlowSize - minGlowSize) * (percent) + minGlowSize);
+        }
+        flashlightPlayerGlowSR.transform.localScale = sizeGlow;
+        if (Utility.between(glowAlpha, 0, 1))
+        {
+            flashlightPlayerGlowSR.color = flashlightPlayerGlowSR.color.adjustAlpha(glowAlpha);
+        }
+        else
+        {
+            flashlightPlayerGlowSR.color = flashlightPlayerGlowSR.color.adjustAlpha(percent * (maxAlpha - minAlpha) + minAlpha);
         }
     }
 
-    void startFade(Vector2 oldPos, Vector2 newPos)
+    void startFade()
     {
-        playerController.Teleport.onTeleport -= startFade;
-        afterglowStartSize = flashlightPlayerGlowSR.transform.localScale.x;//assume x and y are same and the sprite takes a single unit
-        Timer timer = Timer.startTimer(afterglowDuration, () =>
+        if (timer)
+        {
+            timer.TimeLeft = afterglowDuration;
+            Destroy(timer);
+        }
+        timer = Timer.startTimer(afterglowDuration, () =>
         {
             if (!flashlightOn)
             {
                 flashlight.SetActive(false);
+                flashAuraOn = false;
+                afterglowStartSize = 0;
             }
         });
         timer.onTimeLeftChanged += (timeLeft, duration) =>
@@ -127,18 +172,8 @@ public class FlashlightAbility : PlayerAbility
             if (!flashlightOn)
             {
                 float percent = timeLeft / duration;
-                if (Utility.between(glowAlpha, 0, 1))
-                {
-                    flashlightPlayerGlowSR.color = flashlightPlayerGlowSR.color.adjustAlpha(glowAlpha);
-                }
-                else
-                {
-                flashlightPlayerGlowSR.color = flashlightPlayerGlowSR.color.adjustAlpha(percent * (maxAlpha - minAlpha) + minAlpha);
-                }
 
-                Vector2 sizeGlow = flashlightPlayerGlowSR.transform.localScale;
-                sizeGlow = Vector2.one * ((afterglowStartSize) * percent);
-                flashlightPlayerGlowSR.transform.localScale = sizeGlow;
+                updateFlashAuraVisuals(percent, afterglowStartSize);
             }
             else
             {
@@ -151,5 +186,26 @@ public class FlashlightAbility : PlayerAbility
     protected override void acceptUpgradeLevel(AbilityUpgradeLevel aul)
     {
         maxPullBackDistance = aul.stat1;
+    }
+
+    public override SavableObject CurrentState {
+        get => base.CurrentState.more(
+            "flashlightDirection", flashlightDirection,
+            "flashlightOn", flashlightOn,
+            "flashAuraOn", flashAuraOn
+            );
+        set {
+            bool prevlight = flashlightOn;
+            bool prevaura = flashAuraOn;
+            flashlightOn = value.Bool("flashlightOn");
+            flashAuraOn = value.Bool("flashAuraOn");
+            flashlightDirection = value.Vector2("flashlightDirection");
+            if (flashlightOn != prevlight || flashAuraOn != prevaura)
+            {
+                FlashlightDirection = flashlightDirection;
+                updateFlashlightVisuals();
+                startFade();
+            }
+        }
     }
 }
