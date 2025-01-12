f1ee3b3d13c13803a7723b68b1e23bf38b28d4c8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-12 06:51:51 -0500

    [TOOL] Check character setup

--- Assets/Editor/CustomMenu.cs
+++ Assets/Editor/CustomMenu.cs
@@ -620,6 +620,7 @@ public class CustomMenu
                 checkForIllegalPrefabOverrides,
                 checkForGroundLayerObjects,
                 checkTriggersAreNotSolid,
+                ensureNPCsHaveDialogueTriggers,
             }
             .ConvertAll(func =>
             {
@@ -1254,6 +1255,40 @@ public class CustomMenu
         }
 
         return problemCount > 0;
+    }
+
+
+    [MenuItem("SG7/Build/Pre-Build/Ensure NPCs have dialogue set up")]
+    public static bool ensureNPCsHaveDialogueTriggers()
+    {
+        int changedCount = 0;
+        int problemCount = 0;
+        GameObject.FindObjectsByType<Character>(FindObjectsSortMode.None).ToList()
+            .ForEach(chr =>
+            {
+                if (chr)
+                {
+                    DialogueTrigger dt = chr.GetComponent<DialogueTrigger>() ?? chr.GetComponentInChildren<DialogueTrigger>();
+                    if (!dt)
+                    {
+                        Debug.LogError($"Character {chr.gameObject.Name()} needs a DialogueTrigger!", chr);
+                        problemCount++;
+                        return;
+                    }
+                    if (!dt.characters.Contains(chr.characterName))
+                    {
+                        dt.characters.Add(chr.characterName);
+                        Debug.LogWarning($"Character {chr.gameObject.Name()} now has dialogue trigger set up!", chr);
+                        EditorUtility.SetDirty(dt);
+                        changedCount++;
+                    }
+                }
+            });
+        if (problemCount > 0)
+        {
+            Debug.LogError($"There are {problemCount} characters with some setup problems!");
+        }
+        return changedCount > 0 || problemCount > 0;
     }
 
     [MenuItem("SG7/Build/Pre-Build/Populate ObjectManager known objects list")]
