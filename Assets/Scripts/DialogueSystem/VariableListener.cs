7006d27085da91a1f30e136a3b4016cc0a2c34ef
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-13 04:04:01 -0500

    Add VariableListener

---
+++ Assets/Scripts/DialogueSystem/VariableListener.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableListener : MonoBehaviour
+{
+    public List<GameObject> objectsToEnable;
+    public string variableToListenFor;
+    public int minValue;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        Managers.Progress.onVariableChange -= listenForVariableChange;
+        Managers.Progress.onVariableChange += listenForVariableChange;
+
+        listenForVariableChange(variableToListenFor, 0, Managers.Progress.get(variableToListenFor));
+    }
+    private void OnDisable()
+    {
+        Managers.Progress.onVariableChange -= listenForVariableChange;
+    }
+
+    private void listenForVariableChange(string varName, int oldValue, int newValue)
+    {
+        if (varName == variableToListenFor)
+        {
+            bool enable = newValue >= minValue;
+            objectsToEnable.ForEach(go=>go.SetActive(enable));
+        }
+    }
+}
