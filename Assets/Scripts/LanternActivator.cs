1c0a3003f0b8e8223112b6bc6af8d7e42cfa2cb1
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-17 03:43:32 -0500

    Attempt to fix bug: lanterns dont glow*
    *when rewinding into loaded scene

--- Assets/Scripts/LanternActivator.cs
+++ Assets/Scripts/LanternActivator.cs
@@ -36,6 +36,7 @@ public class LanternActivator : MemoryMonoBehaviour
         foreach (ParticleSystem ps in GetComponentsInChildren<ParticleSystem>())
         {
             ps.Play();
+            ps.Emit(10);
         }
         Destroy(this);//delete this script
     }
