756ae8f348f2835db48d07f98b1a21c8e3a40d5d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-18 00:07:25 -0500

    Fix bug: static effect lingering when Merky runs out of charge

--- Assets/Scripts/PlayerController/ElectricBeamAbility.cs
+++ Assets/Scripts/PlayerController/ElectricBeamAbility.cs
@@ -123,6 +123,7 @@ public class ElectricBeamAbility : PlayerAbility
                 if (charge == 0)
                 {
                     Activated = false;
+                    Target = null;
                 }
             }
             else
