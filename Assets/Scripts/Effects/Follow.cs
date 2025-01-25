efe6c3eb4065e503863d6d533bb240416ac97245
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-24 23:37:14 -0500

    #627 Offset tutorial follow position

--- Assets/Scripts/Effects/Follow.cs
+++ Assets/Scripts/Effects/Follow.cs
@@ -11,6 +11,7 @@ public class Follow : MonoBehaviour
 
     public string followName = "";
     public GameObject followObject;
+    public Vector2 startOffset = Vector2.zero;
     public bool orientToCamera = false;
     public bool shakeOnStop = true;
 
@@ -105,7 +106,8 @@ public class Follow : MonoBehaviour
     {
         //Position
         transform.position = followObject.transform.position
-            + ((useOffset) ? (Vector3)offset : Vector3.zero);
+            + ((useOffset) ? (Vector3)offset : Vector3.zero)
+            + (Vector3)startOffset;
         //Rotation
         if (!useCameraUp)
         {
