1c841b7453d4ed97fd6c9ce32dcf1216b14b7ad3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-02-08 04:26:42 -0500

    #634 Interface: ISetupable

---
+++ Assets/Scripts/ISetupable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>
+/// A class is setupable if it has code that needs to run during the pre-build steps
+/// </summary>
+public interface ISetupable
+{
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Setup, returns how many changes were made, 0 if no changes were made
+    /// </summary>
+    /// <returns></returns>
+    public int setup();
+
+#endif
+}
