ed12449cc9dfbaee614dcc67243615c1a2e48f8f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-02-14 01:04:39 -0500

    #638 GameData

--- Assets/Scripts/Core/GameData.cs
+++ Assets/Scripts/Core/GameData.cs
@@ -19,7 +19,7 @@ public class GameData : ICloneable
     // Runtime Vars
     //
     [ES3NonSerializable]
-    public Dictionary<int, GameObject> gameObjects = new Dictionary<int, GameObject>();//list of current objects that have state to save
+    public Dictionary<int, SavableObjectInfo> savables = new Dictionary<int, SavableObjectInfo>();//list of current objects that have state to save
 
     /// <summary>
     /// Stores the object's id and the scene id of the scene that it's in
@@ -36,7 +36,7 @@ public class GameData : ICloneable
         gameData.knownObjects = this.knownObjects.ToList();
         gameData.gameStates = this.gameStates.ToList();
 
-        gameData.gameObjects = this.gameObjects.ToDictionary(entry => entry.Key, entry => entry.Value);
+        gameData.savables = this.savables.ToDictionary(entry => entry.Key, entry => entry.Value);
         gameData.objectSceneList = this.objectSceneList.ToDictionary(entry => entry.Key, entry => entry.Value);
 
 
