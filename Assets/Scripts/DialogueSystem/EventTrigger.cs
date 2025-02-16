aeeeb442fb318b0333d98df056d5fa92b6ef9bc7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-02-16 01:54:43 -0500

    #634 Put NonSolid tags on some classes

--- Assets/Scripts/DialogueSystem/EventTrigger.cs
+++ Assets/Scripts/DialogueSystem/EventTrigger.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[NonSolid]
 [DisallowMultipleComponent]
 [RequireComponent(typeof(Collider2D))]
 public abstract class EventTrigger : MonoBehaviour, ISetupable
--- Assets/Scripts/HiddenArea.cs
+++ Assets/Scripts/HiddenArea.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[NonSolid]
 public class HiddenArea : MemoryMonoBehaviour, ISetupable
 {
 
