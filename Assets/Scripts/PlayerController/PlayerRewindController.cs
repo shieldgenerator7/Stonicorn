6a581559d2fb9dfa3d25c5402c4d353fcf1b033a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-02-16 00:37:47 -0500

    #641 First past merky position adjustment

--- Assets/Scripts/PlayerController/PlayerRewindController.cs
+++ Assets/Scripts/PlayerController/PlayerRewindController.cs
@@ -148,18 +148,13 @@ public class PlayerRewindController : Manager
             //Make sure it's always on screen
             try
             {
-                if (!Managers.Camera.inView(rep.transform.position))
-                {
-                    rep.transform.position =
+                rep.transform.position = (Managers.Camera.inView(gs.Merky.position))
+                    ? gs.Merky.position
+                    :
                         Managers.Camera.getInViewPosition(
                             gs.Merky.position,
                             0.9f
                         );
-                }
-                else
-                {
-                    rep.transform.position = gs.Merky.position;
-                }
             }
             catch (System.NullReferenceException)
             {
