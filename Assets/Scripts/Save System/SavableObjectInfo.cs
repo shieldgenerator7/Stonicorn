83cbab359b1e3169df78def1ad6f1b6d2fafe0ec
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-02-18 00:33:25 -0500

    Change SavableObjectInfo.TextLine

--- Assets/Scripts/Save System/SavableObjectInfo.cs
+++ Assets/Scripts/Save System/SavableObjectInfo.cs
@@ -24,7 +24,7 @@ public class SavableObjectInfo : ObjectInfo, ISetupable
     [AutoInitialize(AllowUnfound =true), SerializeField, HideInInspector]
     public List<SavableMonoBehaviour> savables;
 
-    public string TextLine => $"--{this.name} ({this.Id})--";
+    public string TextLine => $"\"{this.name}\" ({this.Id})";
 
     public SavableObjectInfoData Data
     {
