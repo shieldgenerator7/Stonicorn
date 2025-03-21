4778c04c4dbe3484fa0cbc10996ba693e03fa7de
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-03-21 17:16:42 -0400

    #655 Display NPC progress

---
+++ Assets/Scripts/Menu/ButtonSpecific/ProgressDisplayerNPCs.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ProgressDisplayerNPCs : ProgressDisplayer
+{
+    [Tooltip("When you talk to an NPC for the first time, a variable will be updated. Enter the name of that variable into this list for each NPC.")]
+    public List<string> npcVariableNames;
+
+    protected override int CurrentCount => npcVariableNames.Count(name => Managers.Progress.get(name) > 0);
+
+    protected override int MaxCount => npcVariableNames.Count;
+
+    [Initializer]
+    private List<string> init_npcVariableNames 
+        => FindObjectsByType<Character>(FindObjectsSortMode.InstanceID).ToList()
+            .ConvertAll(chr => $"{chr.characterName.ToLower()}intro")
+            .Distinct().ToList();
+}
