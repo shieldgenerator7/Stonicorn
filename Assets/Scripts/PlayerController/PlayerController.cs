8319cd273b404a70b70278bd2f4d8b7ebe73c4c4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-03-21 20:53:53 -0400

    #634 Revert previous change regarding initializing Merky halfWidth

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -614,7 +614,7 @@ public class PlayerController : MonoBehaviour
 
     //Estimate the halfWidth
     [Initializer("_halfWidth")]
-    private float init__halfWidth => (Mathf.Max(gameObject.getSize(false).x, gameObject.getSize(false).y)) / 2;
+    private float init__halfWidth => ((gameObject.getSize(true).x+ gameObject.getSize(true).y)/2) / 2;
 
 #endif
 }
