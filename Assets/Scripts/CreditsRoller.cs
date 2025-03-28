7bee30b7dc1a80f239219915c8d50c2c75988197
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-03-28 16:42:44 -0400

    #656 Restart backdrop fade when camera zooms*
    *but just keep it faded out if in menu or trying to rewind

--- Assets/Scripts/CreditsRoller.cs
+++ Assets/Scripts/CreditsRoller.cs
@@ -8,11 +8,22 @@ public class CreditsRoller : MonoBehaviour
 
     public float scrollSpeed = 1;
     public RectTransform creditsContainer;
+    public Fader backdropFader;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //camera = Managers.Camera.cam;   
+    }
+
+    private void OnEnable()
+    {
+        Managers.Camera.onZoomLevelChanged -= listenForZoom;
+        Managers.Camera.onZoomLevelChanged += listenForZoom;
+    }
+    private void OnDisable()
+    {
+        Managers.Camera.onZoomLevelChanged -= listenForZoom;
     }
 
     // Update is called once per frame
@@ -22,5 +33,18 @@ public class CreditsRoller : MonoBehaviour
         pos.y += scrollSpeed * Time.unscaledDeltaTime;
         creditsContainer.position = pos;
 
+    }
+
+    private void listenForZoom(float zoom, float delta)
+    {
+        //restart the backdrop fading
+        backdropFader.enabled = false;
+        //if not in menu or time rewinding,
+        if (zoom >= Managers.Camera.toZoomLevel(CameraController.CameraScalePoints.PORTRAIT)
+            && zoom < Managers.Camera.toZoomLevel(CameraController.CameraScalePoints.TIMEREWIND))
+        {
+            //start backdrop fade again
+            backdropFader.enabled = true;
+        }
     }
 }
