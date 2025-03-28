cbd707dca4acad8ee22dc741ef2e0d84cf3e2873
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-03-28 15:54:18 -0400

    #656 Load credits scene after last dialogue

---
+++ Assets/Scripts/DialogueSystem/OnStartLoadScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnStartLoadScene : MonoBehaviour
+{
+    public string sceneName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+}
