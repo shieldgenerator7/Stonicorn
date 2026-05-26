74d22f6a8b27fdeca910475944c7b7fd094cdc71
shieldgenerator7 <shieldgenerator7@yahoo.com>
2026-05-26 00:16:35 -0400

    #664 Play button zooms out to normal zoom level

--- Assets/Scripts/Menu/ButtonSpecific/MenuActionButtonPlay.cs
+++ Assets/Scripts/Menu/ButtonSpecific/MenuActionButtonPlay.cs
@@ -6,18 +6,18 @@ public class MenuActionButtonPlay : MenuActionButton
 {
     public override void activate()
     {
-        if (Managers.Stats.get(Stat.MENU_BUTTON_PLAY) == 0)
-        {
-            //First time:
-            //Zoom out to radius
-            Managers.Camera.ZoomScalePoint = CameraController.CameraScalePoints.RANGE;
-        }
-        else
-        {
+        //if (Managers.Stats.get(Stat.MENU_BUTTON_PLAY) == 0)
+        //{
+        //    //First time:
+        //    //Zoom out to radius
+        //    Managers.Camera.ZoomScalePoint = CameraController.CameraScalePoints.RANGE;
+        //}
+        //else
+        //{
             //Following time:
             //Zoom out to default
             Managers.Camera.ZoomScalePoint = CameraController.CameraScalePoints.DEFAULT;
-        }
+        //}
         Managers.Stats.addOne(Stat.MENU_BUTTON_PLAY);
     }
 }
